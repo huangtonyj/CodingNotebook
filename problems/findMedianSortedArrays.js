38c0baafc85a9caf73f3ef089e77af7e518a7daa
tonyjhuang <huangtonyj91@gmail.com>
2018-11-21 13:16:38 -0800

    init findMedianSortedArrays

---
+++ problems/findMedianSortedArrays.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function (nums1, nums2) {
+  // length of nums1
+  // length of nums2
+  
+};
+
+console.log(findMedianSortedArrays([1,3], [2]))
+console.log(findMedianSortedArrays([1,2], [3,4]))
