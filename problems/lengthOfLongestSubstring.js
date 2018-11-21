d44beeb29329609adc59cc2998d5a0455bf4f187
tonyjhuang <huangtonyj91@gmail.com>
2018-11-21 11:23:45 -0800

    refactor lengthOfLongestSubstring with set instead of hash

--- problems/lengthOfLongestSubstring.js
+++ problems/lengthOfLongestSubstring.js
@@ -4,19 +4,27 @@
  */
 const lengthOfLongestSubstring = function (s) {
   if (s === '') { return 0}
-  if (s === ' ') { return 1}
+  
   let result = 1;
-  let counter = {};
+  let counter = new Set;
+  // let counter = {};
 
   for (i = 0; i < s.length; i++) {
     for (j = i; j < s.length; j++) {
-      if (counter[s[j]] === 1) {
-        result = Math.max(result, Object.values(counter).reduce((acc, el) => acc + el))
-        counter = {};
+      if (counter.has(s[j])) {
+        result = Math.max(result, counter.size)
+        counter = new Set;
         break;
       } else {
-        counter[s[j]] = 1;
+        counter.add(s[j]);
       }
+      // if (counter[s[j]] === 1) {
+      //   result = Math.max(result, Object.values(counter).reduce((acc, el) => acc + el))
+      //   counter = {};
+      //   break;
+      // } else {
+      //   counter[s[j]] = 1;
+      // }
     }
   }
   return result;
