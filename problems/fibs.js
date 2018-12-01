586cb4ec609622e7c1b4607093f459fe4a9b3f57
tonyjhuang <huangtonyj91@gmail.com>
2018-11-30 17:22:20 -0800

    Add Dynamic Programming to fibs

--- problems/fibs.js
+++ problems/fibs.js
@@ -1,4 +1,32 @@
+// Dynamic Programming
+const dp_fibs = (n) => {
+  if (n == 1) {
+    return 0
+  }
+  if (n == 2) {
+    return 1
+  }
 
+  let n2 = 0;
+  let n1 = 1;
+  let n0;
+
+  for (let i = 3; i <= n; i++) {
+    n0 = n1 + n2;
+    n2 = n1;
+    n1 = n0;
+  }
+  return n0
+}
+
+console.log(dp_fibs(2))
+console.log(dp_fibs(3))
+console.log(dp_fibs(4))
+console.log(dp_fibs(5))
+console.log(dp_fibs(6))
+console.log(dp_fibs(7))
+console.log(dp_fibs(8))
+console.log(dp_fibs(45))
 
 // Recursion
 const r_fibs = (n) => {
