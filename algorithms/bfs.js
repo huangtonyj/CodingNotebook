77561ce0b801f7c57421bad851b4ddb83f70d9c2
tonyjhuang <huangtonyj91@gmail.com>
2018-12-05 00:49:11 -0800

    Draw out tree example

--- algorithms/bfs.js
+++ algorithms/bfs.js
@@ -19,13 +19,19 @@ function bfs(node) {
 
 const rootNode = new BTNode(4);
   rootNode.left = new BTNode(2);
-  rootNode.left.left = new BTNode(1);
-  rootNode.left.right = new BTNode(3);
-
+    rootNode.left.left = new BTNode(1);
+    rootNode.left.right = new BTNode(3);
   rootNode.right = new BTNode(6);
-  rootNode.right.left = new BTNode(5);
-  rootNode.right.right = new BTNode(7);
+    rootNode.right.left = new BTNode(5);
+    rootNode.right.right = new BTNode(7);
+//      4
+//    /   \
+//   2     6
+//  / \   / \
+// 1   3 5   7
 
 console.log(bfs(rootNode), [ 4, 2, 6, 1, 3, 5, 7]);
 
 // Future improvements: use ring buffer instead of array for visitedQueue. Shifting is O(n).
+
+
