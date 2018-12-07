bb31c158a3410d6a7d3ef08446ea2187fd7ebc4e
tonyjhuang <huangtonyj91@gmail.com>
2018-12-07 15:35:47 -0800

    Code HeapSort

---
+++ algorithms/heapSort.js
@@ -0,0 +1,19 @@
+const Heap = require('../data_structures/Heap');
+
+function heapSort (arr, sort) {
+  const result = [];
+  const heap = new Heap(sort);
+
+  arr.forEach((el) => heap.insert(el));
+
+  while (heap.length() > 0) {
+    result.push(heap.extract());
+  }
+
+  return result;
+}
+
+// Test
+// const arr = [3,5,4,1,2,0];
+// console.log(heapSort(arr))
+// console.log(heapSort(arr, (a, b) => b - a))
