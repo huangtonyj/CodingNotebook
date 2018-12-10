4fe837db595dcdfbb0024042590333f3a84e1805
tonyjhuang <huangtonyj91@gmail.com>
2018-12-10 15:19:49 -0800

    draft simple Graph class

--- data_structures/graph.js
+++ data_structures/graph.js
@@ -1,37 +1,23 @@
-// Directed Graph with default weight 1
 class Graph {
-  constructor(vertices, edges) {
-    this.vertices = [];
-    this.edges = [];
-
-    vertices.forEach((vertex) => this.add_vertex(vertex))
-    edges.forEach((edge) => this.add_edge(...edge))
+  constructor(nodes) {
+    this.nodes = nodes || {};
   }
 
-  add_vertex(vertex) {
-    this.vertices.push(vertex)
+  addNode(value) {
+    this.nodes[value] = new Array();
   }
 
-  add_edge(from_vertex, to_vertex, cost = 1) {
-    this.edges.push(new Edge(from_vertex, to_vertex, cost))
-  }
-}
-class Vertex {
-  constructor(value) {
-    this.value = value;
-    this.in_edges = [];
-    this.out_edges = [];
-  }
-}
-class Edge {
-  constructor(from_vertex, to_vertex, cost) {
-    this.from_vertex = from_vertex;
-    this.to_vertex = to_vertex;
-    this.cost = cost;
-
-    from_vertex.out_edges.push(this);
-    to_vertex.in_edges.push(this);
+  addEdge(node, edge) {
+    this.nodes[node][edge] = 1;
   }
 }
 
-module.exports = { Graph, Vertex, Edge }
+let myGraph = new Graph();
+myGraph.addNode(4)
+myGraph.addNode(5)
+myGraph.addNode(6)
+
+myGraph.addEdge(4,6)
+myGraph.addEdge(6,5)
+myGraph.addEdge(5,4)
+
