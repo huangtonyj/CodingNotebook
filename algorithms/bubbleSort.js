7055a243a395991c489bcd00e5efe7be0fd91bb9
tonyjhuang <huangtonyj91@gmail.com>
2018-12-12 15:27:02 -0800

    code bubbleSort

---
+++ algorithms/bubbleSort.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr, sortFn) {
+  const sortASC = (a,b) => a - b;
+  sortFn = sortFn || sortASC;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (sortFn(arr[j], arr[j + 1])) {
+        temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+        // console.log(arr);
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([5, 4, 3, 2, 1]), [1, 2, 3, 4, 5]);
+console.log(bubbleSort([1, 2, 3, 4, 5], (a, b) => b - a), [5, 4, 3, 2, 1]);
