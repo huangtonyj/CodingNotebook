e6c2a29e507bbdcaefc9fc576951330fe6c9cfd1
tonyjhuang <huangtonyj91@gmail.com>
2018-12-12 07:57:34 -0800

    code kLargest

---
+++ algorithms/kLargest.js
@@ -0,0 +1,15 @@
+const Heap = require('../data_structures/Heap');
+
+function kLargest(k, arr) {
+  const minHeap = new Heap((a,b) => a-b);
+
+  arr.forEach((el) => {
+    minHeap.insert(el)
+    if (minHeap.length() > k) { minHeap.extract() }
+  })
+
+  return minHeap.store
+}
+
+// O(n log k)
+console.log(kLargest(3, [1,2,3,4,5,6,7,8,9,10]));
