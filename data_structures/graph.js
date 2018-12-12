bd3f946011a46ea53e0c3cfd848707657caaadb5
tonyjhuang <huangtonyj91@gmail.com>
2018-12-12 11:20:16 -0800

    Add cost option to Graph

--- data_structures/graph.js
+++ data_structures/graph.js
@@ -18,15 +18,19 @@ class Graph {
 
     this.nodes[value] = {};
     this.length += 1;
+    
+    return this.nodes;
   }
 
-  addEdge(node, edge) {
+  addEdge(node, edge, cost = 1) {
     if (edge === undefined) {throw 'not enough arguments'}
     if (!this.hasNode(node)) {throw `node ${node} does not exist`}
     if (!this.hasNode(edge)) {throw `node ${edge} does not exist`}
     if (this.hasEdge(node, edge)) {throw `node ${node} with edge ${edge} already exist`}
 
-    this.nodes[node][edge] = true;
+    this.nodes[node][edge] = cost;
+    
+    return this.nodes;
   }
 
   removeNode(node) {
@@ -49,12 +53,16 @@ class Graph {
     delete this.nodes[node][edge]
   }
 
+  length() {
+    return this.length;
+  }
+
 }
 
 module.exports = Graph;
 
 // let myGraph = new Graph();
-// myGraph.addNode(4)
+// myGraph.addNode(4, 1)
 // myGraph.addNode(5)
 // myGraph.addNode(6)
 // myGraph.addEdge(4,6)
@@ -63,7 +71,7 @@ module.exports = Graph;
 
 let myGraph2 = new Graph(
   {
-    4:{6: true},
-    5:{4: true},
-    6:{5: true},
+    4:{6: 1},
+    5:{4: 1},
+    6:{5: 1},
 })
