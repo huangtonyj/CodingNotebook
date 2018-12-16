c48d59fde8d5365b49a60ab418925b0a3d429c46
tonyjhuang <huangtonyj91@gmail.com>
2018-12-16 15:23:39 -0800

    draft runningMedian

--- problems/runningMedian.js
+++ problems/runningMedian.js
@@ -1,32 +1,35 @@
+const Heap = require('../data_structures/Heap');
 const quickSort = require('../algorithms/quickSort');
 
 class runningMedian {
   constructor() {
     this.store = [];
-    this.length = 0; // keep track of odd or even
+    this.effectiveMedian = null;
   }
 
   add(value) {
     // binary insertion?
-    // return median
-
     this.store.push(value)
-    this.length += 1;
     this.store = quickSort(this.store);
 
     return this.median()
   }
 
   median() {
-    return this.store;
-    return 'median'
+    const mid = Math.floor(this.store.length / 2);
+
+    if (this.store.length & 1) {
+      return this.store[mid];
+    } else {
+      return (this.store[mid] + this.store[mid-1]) / 2;
+    }
   }
 }
 
 const rM = new runningMedian();
-  console.log(rM.add(12));
-  console.log(rM.add(4));
-  console.log(rM.add(5));
-  console.log(rM.add(3));
-  console.log(rM.add(8));
-  console.log(rM.add(7));
+  console.log(rM.add(12), 12);
+  console.log(rM.add(4), 8);
+  console.log(rM.add(5), 5);
+  console.log(rM.add(3), 4.5);
+  console.log(rM.add(8), 5);
+  console.log(rM.add(7), 6);
