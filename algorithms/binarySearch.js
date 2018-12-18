9d13f830c70fa860b9fd233f40fc2bc1b35e237c
tonyjhuang <huangtonyj91@gmail.com>
2018-12-18 00:47:27 -0800

    clean up code

--- algorithms/binarySearch.js
+++ algorithms/binarySearch.js
@@ -1,12 +1,12 @@
 function binarySearch(arr, target) {
-  if (arr.length <= 1 && arr[0] !== target) { return null}
+  if (arr.length <= 1 && arr[0] !== target) { return null; }
   
   const mid = Math.floor(arr.length / 2);
 
   if (target === arr[mid]) {
-    return mid    
+    return mid;   
   } else if (target < arr[mid]) {
-    return binarySearch(arr.slice(0, mid), target)
+    return binarySearch(arr.slice(0, mid), target);
   } else if (target > arr[mid]) {
     let ans = binarySearch(arr.slice(mid), target);
     return ans ? ans + mid : null;
@@ -16,17 +16,17 @@ function binarySearch(arr, target) {
 }
 
 // TEST
-console.log(binarySearch([1,2,3], 1), 0);
-console.log(binarySearch([1,2,3], 2), 1);
-console.log(binarySearch([1,2,3], 3), 2);
+// console.log(binarySearch([1,2,3], 1), 0);
+// console.log(binarySearch([1,2,3], 2), 1);
+// console.log(binarySearch([1,2,3], 3), 2);
 
-console.log(binarySearch([1,2,3], 0), null);
-console.log(binarySearch([1,2,3], 4), null);
+// console.log(binarySearch([1,2,3], 0), null);
+// console.log(binarySearch([1,2,3], 4), null);
 
-console.log(binarySearch([1,2,3,4], 1), 0);
-console.log(binarySearch([1,2,3,4], 2), 1);
-console.log(binarySearch([1,2,3,4], 3), 2);
-console.log(binarySearch([1,2,3,4], 4), 3);
+// console.log(binarySearch([1,2,3,4], 1), 0);
+// console.log(binarySearch([1,2,3,4], 2), 1);
+// console.log(binarySearch([1,2,3,4], 3), 2);
+// console.log(binarySearch([1,2,3,4], 4), 3);
 
-console.log(binarySearch([1,2,3,4], 0), null);
-console.log(binarySearch([1,2,3,4], 5), null);
+// console.log(binarySearch([1,2,3,4], 0), null);
+// console.log(binarySearch([1,2,3,4], 5), null);
