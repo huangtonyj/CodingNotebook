3cc74015119ea386206bcc8397f86510f7d98eea
tonyjhuang <huangtonyj91@gmail.com>
2019-01-07 00:43:49 -0800

    code interpolationSearch

--- algorithms/interpolationSearch.js
+++ algorithms/interpolationSearch.js
@@ -2,10 +2,8 @@ function interpolationSearch (arr, k) {
   let min = arr[0];
   let max = arr[arr.length-1];
 
-  // let idx = Math.floor((k - min) / (max - min));
-  let idx = Math.floor((k - min) / (max - min) * arr.length);
-  // console.log('min', min, 'max', max, 'k - min', k - min);
-  console.log('idx', idx);
+  let idx = Math.floor((k - min) / (max - min) * (arr.length - 1));
+  // console.log('min', min, 'max', max, '(k - min)', k - min, '(max - min)', (max - min), 'idx', idx);
   
   if (arr[idx] === k) { return true; }
   if (arr[idx] < k) { return interpolationSearch(arr.slice(0, idx), k); }
@@ -14,7 +12,11 @@ function interpolationSearch (arr, k) {
   return false;
 }
 
-
-console.log(interpolationSearch([0,1,2,3,4,5,6,7,8,9], 4));
-console.log(interpolationSearch([2,4,6,8,10], 10));
-console.log(interpolationSearch([2,6,8,10,12,14,16,18,20], 10));
+console.log(interpolationSearch([2, 4, 6, 8, 10], 2), true);
+console.log(interpolationSearch([2, 4, 6, 8, 10], 8), true);
+console.log(interpolationSearch([2, 4, 6, 8, 10], 10), true);
+console.log(interpolationSearch([2, 4, 6, 7, 8, 10], 2), true);
+console.log(interpolationSearch([2, 4, 6, 7, 8, 10], 10), true);
+console.log(interpolationSearch([2, 4, 6, 7, 8, 10], 6), true);
+console.log(interpolationSearch([2, 4, 6, 7, 8, 10], 7), true);
+console.log(interpolationSearch([2, 4, 6, 8, 10], 7), false);
