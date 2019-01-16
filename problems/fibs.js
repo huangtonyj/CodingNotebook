2b7dfd0e8c19f13a3ec19c01a51940329f09c89a
tonyjhuang <huangtonyj91@gmail.com>
2019-01-16 00:46:59 -0800

    refactior fibs

--- problems/fibs.js
+++ problems/fibs.js
@@ -1,11 +1,7 @@
 // Dynamic Programming
 const dp_fibs = (n) => {
-  if (n == 1) {
-    return 0
-  }
-  if (n == 2) {
-    return 1
-  }
+  if (n == 1) return 0;
+  if (n == 2) return 1;
 
   let n2 = 0;
   let n1 = 1;
@@ -16,32 +12,32 @@ const dp_fibs = (n) => {
     n2 = n1;
     n1 = n0;
   }
-  return n0
-}
 
-console.log(dp_fibs(2))
-console.log(dp_fibs(3))
-console.log(dp_fibs(4))
-console.log(dp_fibs(5))
-console.log(dp_fibs(6))
-console.log(dp_fibs(7))
-console.log(dp_fibs(8))
-console.log(dp_fibs(45))
+  return n0;
+};
+
+console.log(dp_fibs(2));
+console.log(dp_fibs(3));
+console.log(dp_fibs(4));
+console.log(dp_fibs(5));
+console.log(dp_fibs(6));
+console.log(dp_fibs(7));
+console.log(dp_fibs(8));
+console.log(dp_fibs(45));
 
 // Recursion
 const r_fibs = (n) => {
-  if (n == 1) { return 0 }
-  if (n == 2) { return 1 }
-  return r_fibs(n-1) + r_fibs(n-2)
-}
+  if (n == 1) return 0;
+  if (n == 2) return 1;
+  return r_fibs(n-1) + r_fibs(n-2);
+};
 
-
-console.log(r_fibs(2))
-console.log(r_fibs(3))
-console.log(r_fibs(4))
-console.log(r_fibs(5))
-console.log(r_fibs(6))
-console.log(r_fibs(7))
-console.log(r_fibs(8))
-console.log(r_fibs(45))
+console.log(r_fibs(2));
+console.log(r_fibs(3));
+console.log(r_fibs(4));
+console.log(r_fibs(5));
+console.log(r_fibs(6));
+console.log(r_fibs(7));
+console.log(r_fibs(8));
+console.log(r_fibs(45));
 
