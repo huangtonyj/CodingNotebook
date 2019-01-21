1fc26522077ad2169083a84c5494466edaa328d6
tonyjhuang <huangtonyj91@gmail.com>
2019-01-21 00:42:28 -0800

    code lakeVolume

--- problems/lakeVolume.js
+++ problems/lakeVolume.js
@@ -1,30 +1,47 @@
+// https://techdevguide.withgoogle.com/paths/advanced/sequence-2/volume-of-water/#!
+
 function lakeVolume(arr) {
 
-  // let prevMaxElevation = arr[0];
-  // let prevMaxElevation = -Infinity;
-  let prevMaxElevation = 0;
   let totalLakeVol = 0;
   let currentLakeVol = 0;
+  let prevMaxElevation = 0;
 
   arr.forEach(currentElevation => {
     if (currentElevation > prevMaxElevation) {
       prevMaxElevation = currentElevation;
       
       totalLakeVol += currentLakeVol;
+
       currentLakeVol = 0;
+      // lakeWidth = 0;
+    } else {
+      currentLakeVol += prevMaxElevation - currentElevation;
+      // lakeWidth += 1;
+    }
+  });
+
+  currentLakeVol = 0;
+  prevMaxElevation = 0;
+
+  arr.reverse().forEach(currentElevation => {
+    if (currentElevation > prevMaxElevation) {
+      prevMaxElevation = currentElevation;
+      
+      totalLakeVol += currentLakeVol;
+
+      currentLakeVol = 0;
+      // lakeWidth = 0;
     } else {
       currentLakeVol += prevMaxElevation - currentElevation;
     }
-    console.log('prevMaxElevation', prevMaxElevation, 'currentElevation', currentElevation, 'totalLakeVol', totalLakeVol, 'currentLakeVol', currentLakeVol);
     
   });
 
   return totalLakeVol;
 
-
-
 }
 
 console.log(lakeVolume([1, 3, 2, 4, 1, 3, 1, 4, 5, 2, 2, 1, 4, 2, 2], 15));
+console.log(lakeVolume([2, 2, 4, 1, 2, 2, 5, 4, 1, 3, 1, 4, 2, 3, 1], 15));
 
 
