9a392d4cdc2b6a24a90a13593c338631af72937d
tonyjhuang <huangtonyj91@gmail.com>
2019-01-21 00:43:46 -0800

    Clean up and add comments

--- problems/lakeVolume.js
+++ problems/lakeVolume.js
@@ -9,39 +9,30 @@ function lakeVolume(arr) {
   arr.forEach(currentElevation => {
     if (currentElevation > prevMaxElevation) {
       prevMaxElevation = currentElevation;
-      
       totalLakeVol += currentLakeVol;
-
       currentLakeVol = 0;
-      // lakeWidth = 0;
     } else {
       currentLakeVol += prevMaxElevation - currentElevation;
-      // lakeWidth += 1;
     }
   });
 
+  // Compute it reverse as well
   currentLakeVol = 0;
   prevMaxElevation = 0;
 
   arr.reverse().forEach(currentElevation => {
     if (currentElevation > prevMaxElevation) {
       prevMaxElevation = currentElevation;
-      
       totalLakeVol += currentLakeVol;
-
       currentLakeVol = 0;
-      // lakeWidth = 0;
     } else {
       currentLakeVol += prevMaxElevation - currentElevation;
     }
-    
   });
 
   return totalLakeVol;
-
 }
 
 console.log(lakeVolume([1, 3, 2, 4, 1, 3, 1, 4, 5, 2, 2, 1, 4, 2, 2], 15));
-console.log(lakeVolume([2, 2, 4, 1, 2, 2, 5, 4, 1, 3, 1, 4, 2, 3, 1], 15));
 
 
