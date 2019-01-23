f8f0d67df0b9da2ebf36a2bc8ffeb2e7ca19aa11
tonyjhuang <huangtonyj91@gmail.com>
2019-01-23 13:50:34 -0800

    draft radixSort

--- algorithms/radixSort.js
+++ algorithms/radixSort.js
@@ -1,26 +1,46 @@
-const quickSort = require('./quickSort');
-
 function radixSort(arr) {
 
-  let buckets = new Array(10).fill([]);
-  console.log(buckets);
-  
-  let tens;
+  const max = Math.max(...arr);
+  let expPower = 10 ** max.toString().length;
+
+  // const buckets = new Array(10).fill(new Array(), 0, 10);
+  const buckets = [[],[], [], [], [], [], [], [], [], []];
+
+  // while (expPower >= 0) {
+  //   expPower -= 1;
+  // }
 
   arr.forEach(el => {
-    tens = Math.floor(el / 10);
-    buckets[tens].push(el);
+    buckets[el % expPower].push(el);
   });
-
-  console.log(buckets);
   
-  // buckets = buckets.map(bucket => quickSort(bucket));
-
   return buckets;
 }
 
 
-const r = () => Math.floor(Math.random() * 100);
-const arr = new Array(10).fill(0, 0, 100).map(el => r());
+const r = () => Math.floor(Math.random() * 10);
+const arr = new Array(25).fill(0, 0, 25).map(el => r());
 
 console.log(radixSort(arr));
+
+
+// const quickSort = require('./quickSort');
+
+// function radixSort(arr) {
+
+//   let buckets = new Array(10).fill([]);
+//   console.log(buckets);
+
+//   let tens;
+
+//   arr.forEach(el => {
+//     tens = Math.floor(el / 10);
+//     buckets[tens].push(el);
+//   });
+
+//   console.log(buckets);
+
+//   // buckets = buckets.map(bucket => quickSort(bucket));
+
+//   return buckets;
+// }
