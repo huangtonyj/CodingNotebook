a35a7cc2ea72731c3df166d7279a95fd1ff2a9e2
tonyjhuang <huangtonyj91@gmail.com>
2019-01-24 12:05:03 -0800

    code longestSubstringWithoutDuplication

--- problems/longestSubstringWithoutDuplication.js
+++ problems/longestSubstringWithoutDuplication.js
@@ -1,5 +1,28 @@
 function longestSubstringWithoutDuplication(str) {
 
+  let longest = [];
+  let current = [];
+  let currentStartIdx = 0;
+  let idxHash = {};
+  let iLetter;
+
+  for (let i = 0; i < str.length; i++) {
+    iLetter = str[i];
+
+    if (idxHash[iLetter]) {
+      if (current.length > longest.length) { longest = current; }
+      current = current.slice(idxHash[iLetter] - currentStartIdx + 1);
+      currentStartIdx = i;
+    // } else {
+    }
+    current.push(iLetter);
+
+    idxHash[iLetter] = i;
+    console.log(longest, current, idxHash, currentStartIdx);
+    
+  }
+
+  return longest.join('');
 }
 
 console.log(longestSubstringWithoutDuplication('clementisacap') === 'mentisac');
