a2e1697904e335cab41f56f4e20ad0ce1b9a36a3
tonyjhuang <huangtonyj91@gmail.com>
2019-01-24 15:20:46 -0800

    code helper function findMin

--- problems/nodeSuccessorBST.js
+++ problems/nodeSuccessorBST.js
@@ -1,12 +1,19 @@
 const NodeBT = require('../data_structures/NodeBT');
 // https://github.com/appacademy/job-search-guide/blob/master/pairboarding-workshop/w4/thursday/partner-a.md
 
-function BSTNodeSuccessor(root, key) {
-// in order dfs and return next?
-  if (root === null) return [];
+function BSTNodeSuccessor(root, parents = []) {
+  // in order dfs and return next?
+  // if (root === null) return [];
+  // if (root.value !== null) {console.log(root.value);}
 
-  return BSTNodeSuccessor(root.left).concat([root.value], BSTNodeSuccessor(root.right));
+
+  // return BSTNodeSuccessor(root.left).concat([root.value], BSTNodeSuccessor(root.right));
+
+  if (root.right) { return findMin(root.right); }
+
 }
+
+const findMin = (node) => node.left ? findMin(node.left) : node;
 
 //       15
 //     /    \
@@ -21,10 +28,24 @@ const myBST = new NodeBT(15);
     myBST.right.left = new NodeBT(16);
     myBST.right.right = new NodeBT(25);
 
-console.log(BSTNodeSuccessor(myBST,  8), 10);
-console.log(BSTNodeSuccessor(myBST, 10), 12);
-console.log(BSTNodeSuccessor(myBST, 12), 15);
-console.log(BSTNodeSuccessor(myBST, 15), 16);
-console.log(BSTNodeSuccessor(myBST, 16), 20);
-console.log(BSTNodeSuccessor(myBST, 20), 25);
-console.log(BSTNodeSuccessor(myBST, 25), null);
+console.log(findMin(myBST.right.right));
+// console.log(BSTNodeSuccessor(myBST,  8), 10);
+// console.log(BSTNodeSuccessor(myBST, 10), 12);
+// console.log(BSTNodeSuccessor(myBST, 12), 15);
+// console.log(BSTNodeSuccessor(myBST, 15), 16);
+// console.log(BSTNodeSuccessor(myBST, 16), 20);
+// console.log(BSTNodeSuccessor(myBST, 20), 25);
+// console.log(BSTNodeSuccessor(myBST, 25), null);
+
+
+
+// function BSTNodeSuccessor(root, key) {
+//   // in order dfs and return next?
+//   if (root === null) return [];
+//   if (root.value !== null) {
+//     console.log(root.value);
+//   }
+
+
+//   return BSTNodeSuccessor(root.left).concat([root.value], BSTNodeSuccessor(root.right));
+// }
