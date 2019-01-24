2f19b4b9c6b8a4cd5d5afb2eb0aea268243886e3
tonyjhuang <huangtonyj91@gmail.com>
2019-01-23 16:50:23 -0800

    refactor radixSort

--- algorithms/radixSort.js
+++ algorithms/radixSort.js
@@ -1,21 +1,20 @@
 function radixSort(arr, digits) {
   if (arr.length < 2) { return arr }
 
-  // Get max num of digits if not specified.
-  digits = digits || 10 ** (Math.max(...arr).toString().length - 1);
+  digits = digits || 10 ** (Math.max(...arr).toString().length - 1); // Get max num of digits if not specified.
   
   let buckets = [[],[], [], [], [], [], [], [], [], []];
-  let iBucket = (num) => Math.floor(num / digits % 10);
+  let iBucket = (num) => Math.floor(num / digits % 10); 
 
-  arr.forEach(el => buckets[iBucket(el)].push(el));
+  arr.forEach(el => buckets[iBucket(el)].push(el)); // assign each el to its respective bucket number
 
-  console.log('arr', arr, 'digits', digits, 'buckets', buckets);
-  if (digits > 1) { buckets = buckets.map(bucket => radixSort(bucket, digits / 10)) }
+  // console.log('arr', arr, 'digits', digits, 'buckets', buckets);
+  if (digits > 1) { buckets = buckets.map(bucket => radixSort(bucket, digits / 10)) } // Recursive radixSort until the ones digit.
 
   return buckets.flat();
 }
 
-const r = () => Math.floor(10 ** (Math.random() * 5));
-const arr = new Array(25).fill(0, 0, Infinity).map(el => r());
+const randromLogNum = () => Math.floor(10 ** (Math.random() * 5));
+const arr = new Array(100).fill(0, 0, Infinity).map(el => randromLogNum());
 
 console.log(radixSort(arr));
