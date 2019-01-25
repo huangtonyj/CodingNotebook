9356ab0adef247d6159cbcc64a280e6cee8db3d3
tonyjhuang <huangtonyj91@gmail.com>
2019-01-24 16:16:58 -0800

    draft BSTNodeSuccessor

--- problems/nodeSuccessorBST.js
+++ problems/nodeSuccessorBST.js
@@ -1,41 +1,45 @@
 const NodeBT = require('../data_structures/NodeBT');
 // https://github.com/appacademy/job-search-guide/blob/master/pairboarding-workshop/w4/thursday/partner-a.md
 
-function BSTNodeSuccessor(root, parents = []) {
-  // in order dfs and return next?
-  // if (root === null) return [];
-  // if (root.value !== null) {console.log(root.value);}
+function BSTNodeSuccessor(node, key, parents = []) {
+  if (key === node.value) { 
+    return node.left ? findBSTMin(node.right).value : findBSTMinAncestor(parents).value;
+  }
 
-
-  // return BSTNodeSuccessor(root.left).concat([root.value], BSTNodeSuccessor(root.right));
-
-  if (root.right) { return findMin(root.right); }
-
+  if (key < node.value) { 
+    parents.push(node);
+    return BSTNodeSuccessor(node.left, key, parents); }
+  if (key > node.value) { return BSTNodeSuccessor(node.right, key, parents); }
+  
 }
 
-const findMin = (node) => node.left ? findMin(node.left) : node;
+const findBSTMin = (node) => node.left ? findBSTMin(node.left) : node;
+const findBSTMinAncestor = (ancestors) => {
+  return ancestors.reduce((acc, el) => {
+    return el.value < acc.value ? el : acc;
+  });
+};
 
-//       15
-//     /    \
-//   10      20
-//  /  \     / \
-// 8   12  16   25
-const myBST = new NodeBT(15);
-  myBST.left = new NodeBT(10);
-    myBST.left.left = new NodeBT(8);
-    myBST.left.right = new NodeBT(12);
-  myBST.right = new NodeBT(20);
-    myBST.right.left = new NodeBT(16);
-    myBST.right.right = new NodeBT(25);
+//       4
+//     /   \
+//    2     6
+//  /  \   / \
+// 1    3 5   7
+const myBST = new NodeBT(4);
+  myBST.left = new NodeBT(2);
+    myBST.left.left = new NodeBT(1);
+    myBST.left.right = new NodeBT(3);
+  myBST.right = new NodeBT(6);
+    myBST.right.left = new NodeBT(5);
+    myBST.right.right = new NodeBT(7);
 
-console.log(findMin(myBST.right.right));
-// console.log(BSTNodeSuccessor(myBST,  8), 10);
-// console.log(BSTNodeSuccessor(myBST, 10), 12);
-// console.log(BSTNodeSuccessor(myBST, 12), 15);
-// console.log(BSTNodeSuccessor(myBST, 15), 16);
-// console.log(BSTNodeSuccessor(myBST, 16), 20);
-// console.log(BSTNodeSuccessor(myBST, 20), 25);
-// console.log(BSTNodeSuccessor(myBST, 25), null);
+console.log(BSTNodeSuccessor(myBST, 1), 2);
+console.log(BSTNodeSuccessor(myBST, 2), 3);
+console.log(BSTNodeSuccessor(myBST, 3), 4);
+console.log(BSTNodeSuccessor(myBST, 4), 5);
+console.log(BSTNodeSuccessor(myBST, 5), 6);
+console.log(BSTNodeSuccessor(myBST, 6), 7);
+// console.log(BSTNodeSuccessor(myBST, 7), null);
 
 
 
