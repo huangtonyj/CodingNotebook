8cf8d641c72f19173a822ba66f8a911c159ad14d
tonyjhuang <huangtonyj91@gmail.com>
2019-01-26 22:08:17 -0800

    write test for longestCommonSubsequence

---
+++ problems/longestCommonSubsequence.js
@@ -0,0 +1,9 @@
+// https://www.dailycodingproblem.com/solution/209?token=fce286c30aaaa7ee9bba1af494bb2e886850e1851a9d964bb2b7403d05587f864ac8bd3a
+
+function longestCommonSubsequence(strA, strB) {
+
+
+}
+
+console.log(longestCommonSubsequence('refrigeration', 'edidemiologist'), 5); // eieio
+console.log(longestCommonSubsequence('refrigeration', 'edidemiologistn'), 6); // eieion
