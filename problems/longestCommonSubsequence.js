338ed51d6053486df1e096d9b301302f48c9927d
tonyjhuang <huangtonyj91@gmail.com>
2019-01-30 21:42:58 -0800

    draft longestCommonSubsequence

--- problems/longestCommonSubsequence.js
+++ problems/longestCommonSubsequence.js
@@ -1,7 +1,14 @@
 // https://www.dailycodingproblem.com/solution/209?token=fce286c30aaaa7ee9bba1af494bb2e886850e1851a9d964bb2b7403d05587f864ac8bd3a
 
 function longestCommonSubsequence(strA, strB) {
+  // 2D matrix
 
+  strA = strA.split('').entries();
+  strB = strB.split('').entries();
+
+  for (let a of strA) {
+    console.log(a);
+  }
 
 }
 
