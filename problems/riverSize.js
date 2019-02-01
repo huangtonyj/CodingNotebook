45fa5e8bacfa98a6c19dad69054879f9694ec831
tonyjhuang <huangtonyj91@gmail.com>
2019-02-01 13:49:35 -0800

    write test case for riverSize

---
+++ problems/riverSize.js
@@ -0,0 +1,13 @@
+// https://github.com/appacademy/job-search-guide/blob/master/pairboarding-workshop/w6/tuesday/partner-a.md
+
+function riverSize(matrix) {
+
+}
+
+console.log(riverSize([
+  [1, 0, 0, 1, 0],
+  [1, 0, 1, 0, 0],
+  [0, 0, 1, 0, 1],
+  [1, 0, 1, 0, 1],
+  [1, 0, 1, 1, 0]
+]), [1, 2, 2, 2, 5]);
