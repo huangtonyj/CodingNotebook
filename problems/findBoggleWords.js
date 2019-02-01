f2020e72860349ccaf2503daa4e3b903c256119a
tonyjhuang <huangtonyj91@gmail.com>
2019-02-01 15:53:11 -0800

    write test for findBoggleWords

---
+++ problems/findBoggleWords.js
@@ -0,0 +1,12 @@
+function findBoggleWords(dictionary, boggle) {
+
+}
+
+const dictionary = ["GEEKS", "FOR", "QUIZ", "GO"];
+const boggle = [
+  ['G', 'I', 'Z'],
+  ['U', 'E', 'K'],
+  ['Q', 'S', 'E']
+];
+
+console.log(findBoggleWords(dictionary, boggle), ['GEEKS', 'QUIZ']);
