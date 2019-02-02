9cb3deafe5b353cea14dcc73e1f2753201ae90cb
tonyjhuang <huangtonyj91@gmail.com>
2019-02-02 13:58:51 -0800

    Set up Trie skeleton

---
+++ data_structures/Trie.js
@@ -0,0 +1,21 @@
+class TrieNode {
+  constructor(value) {
+    this.value = value;
+    this.children = new Array(26);
+    this.isEndOfWord = false;
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = {};
+  }
+
+  insert(word){
+    
+  }
+
+  search(word){
+
+  }
+}
