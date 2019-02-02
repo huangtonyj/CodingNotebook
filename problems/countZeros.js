002c71ff12dbe642d13633be01911abac9b0476b
tonyjhuang <huangtonyj91@gmail.com>
2019-02-02 15:40:22 -0800

    create countZeros skeleton + test cases

---
+++ problems/countZeros.js
@@ -0,0 +1,9 @@
+// Count Total number of zeros from 1 upto n ?
+
+function countZeros(n) {
+
+}
+
+console.log(countZeros(10), 1);
+console.log(countZeros(100), 11);
+console.log(countZeros(2014), 223);
