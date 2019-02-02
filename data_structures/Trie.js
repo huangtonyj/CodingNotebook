ed32d174166e0efe20ffacba42179fa46dafae56
tonyjhuang <huangtonyj91@gmail.com>
2019-02-02 14:46:37 -0800

    code insert method

--- data_structures/Trie.js
+++ data_structures/Trie.js
@@ -12,10 +12,28 @@ class Trie {
   }
 
   insert(word){
-    
+    word = word.split("");
+
+    let currentNode = this.root;
+
+    while (word.length > 0) {
+      const currentLetter = word.shift();
+
+      if (!currentNode[currentLetter]) currentNode[currentLetter] = {};
+      currentNode = currentNode[currentLetter];
+    }
+
+    currentNode['isEndOfAWord'] = true;
   }
 
   search(word){
 
   }
 }
+
+const myTrie = new Trie();
+  myTrie.insert('the');
+  myTrie.insert('these');
+  // myTrie.insert('their');
+  // myTrie.insert('thaw');
+
