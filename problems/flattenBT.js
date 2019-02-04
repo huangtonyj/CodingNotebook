5edd0d471b14ba346a37d89b12de306b3381aab8
tonyjhuang <huangtonyj91@gmail.com>
2019-02-04 12:47:17 -0800

    code flattenBT

--- problems/flattenBT.js
+++ problems/flattenBT.js
@@ -3,9 +3,22 @@ const NodeBT = require('../data_structures/NodeBT');
 const NodeLL = require('../data_structures/NodeLL');
 
 function flattenBT(node) {
-  if (!node) {return []; }
+  if (!node) {return ''; }
 
-  return [node.value].concat(flattenBT(node.left), flattenBT(node.right));
+  const LLNode = new NodeLL(node.value);
+  const left = flattenBT(node.left);
+  const right = flattenBT(node.right);
+
+  let currentLeft = left;
+
+  while(currentLeft.next) {
+    currentLeft = currentLeft.next;
+  }
+
+  currentLeft.next = right;
+  LLNode.next = left;
+
+  return LLNode;
 }
 
 //       1
@@ -20,12 +33,9 @@ const myBT = new NodeBT(1);
   myBT.right = new NodeBT(5);
     myBT.right.right = new NodeBT(6);
 
+let currentNode = flattenBT(myBT);
 
-console.log(flattenBT(myBT));
-
-// let currentNode = flattenBT(myBT);
-
-// while(currentNode) {
-//   console.log(currentNode.value);
-//   currentNode = currentNode.next; 
-// }
+while(currentNode) {
+  console.log(currentNode.value);
+  currentNode = currentNode.next; 
+}
