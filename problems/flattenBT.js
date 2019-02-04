b7b6db68cd215b68d1651eec69fd8ea37330f2c5
tonyjhuang <huangtonyj91@gmail.com>
2019-02-04 11:27:05 -0800

    flattenBT skeleton

---
+++ problems/flattenBT.js
@@ -0,0 +1,29 @@
+// Given a binary tree, flatten it to a linked list.
+const NodeBT = require('../data_structures/NodeBT');
+const NodeLL = require('../data_structures/NodeLL');
+
+function flattenBT(node) {
+
+}
+
+//       1
+//     /   \
+//    2     5
+//  /  \     \
+// 3    4     6
+const myBT = new NodeBT(1);
+  myBT.left = new NodeBT(2);
+    myBT.left.left = new NodeBT(3);
+    myBT.left.right = new NodeBT(4);
+  myBT.right = new NodeBT(5);
+    myBT.right.right = new NodeBT(6);
+
+
+console.log(flattenBT(myBT));
+
+// let currentNode = flattenBT(myBT);
+
+// while(currentNode) {
+//   console.log(currentNode.value);
+//   currentNode = currentNode.next; 
+// }
