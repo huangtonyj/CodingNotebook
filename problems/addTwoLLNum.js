26768defb411d99092e52f8d7fb06a3ff71e3bb9
tonyjhuang <huangtonyj91@gmail.com>
2019-02-04 14:11:27 -0800

    fix bug with different LL lengths

--- problems/addTwoLLNum.js
+++ problems/addTwoLLNum.js
@@ -2,9 +2,9 @@
 // The digits are stored in reverse order and each of their nodes contain a single digit. 
 // Add the two numbers and return it as a linked list.
 
-// Input: (2 - > 4 - > 3) + (5 - > 6 - > 4)
-// Output: 7 - > 0 - > 8
-// Explanation: 342 + 465 = 807.
+// Input: (2 - > 4 - > 3) + (5 - > 6 - > 4 -> 1)
+// Output: 7 - > 0 - > 8 -> 1
+// Explanation: 342 + 465 = 1807.
 
 const nodeLL = require('../data_structures/NodeLL');
 
@@ -15,13 +15,15 @@ function addTwoLLNum(LL1, LL2) {
   let nodeLL1 = LL1;
   let nodeLL2 = LL2;
 
-  while (nodeLL1) {
+  while (nodeLL1 || nodeLL2) {
+    const val1 = nodeLL1 ? nodeLL1.value : 0;
+    const val2 = nodeLL2 ? nodeLL2.value : 0;
 
-    ans += tensFactor * (nodeLL1.value + nodeLL2.value);
+    ans += tensFactor * (val1 + val2);
 
     tensFactor *= 10;
-    nodeLL1 = nodeLL1.next;
-    nodeLL2 = nodeLL2.next;
+    nodeLL1 = nodeLL1 ? nodeLL1.next : nodeLL1;
+    nodeLL2 = nodeLL2 ? nodeLL2.next : nodeLL2;
   }
 
   return ans;
@@ -34,5 +36,6 @@ const LL1 = new nodeLL(2);
 const LL2 = new nodeLL(5);
   LL2.next = new nodeLL(6);
   LL2.next.next = new nodeLL(4);
+  LL2.next.next.next = new nodeLL(1);
 
-console.log(addTwoLLNum(LL1, LL2), 807);
+console.log(addTwoLLNum(LL1, LL2), 1807);
