11542306dede1ac9661454e50407784eda02dbb0
tonyjhuang <huangtonyj91@gmail.com>
2019-02-06 16:30:25 -0800

    code removeNthFromEnd

--- problems/removeNthFromEnd.js
+++ problems/removeNthFromEnd.js
@@ -1,10 +1,25 @@
 const NodeLL = require ('../data_structures/nodeLL');
 
 function removeNthFromEnd(head, n) {
+  let currentNode = head;
+  let currentNodePlusN = head;
+
+  for (let i = 0; i < n; i++) {
+    currentNodePlusN = currentNodePlusN.next;
+  }
+  
+  while (currentNodePlusN.next) {
+    currentNode = currentNode.next;
+    currentNodePlusN = currentNodePlusN.next;
+  }
+
+  currentNode.next = currentNode.next.next;
 
   return head;
 }
 
+
+// Test case
 const myLL = new NodeLL(10);
   
 let currentNode = myLL;
@@ -17,10 +32,10 @@ currentNode = removeNthFromEnd(myLL, 3);
 let i = 10;
 while(currentNode) {
   if (i !== 3) { 
-    console.log(currentNode.value, i ); 
+    console.log(currentNode.value === i, currentNode.value, i); 
     currentNode = currentNode.next;
   } else {
-    console.log(currentNode.value, 'not 3');
+    console.log(currentNode.value !== 3, currentNode.value, '2 not 3'); // deleted 3
   }
   i--;
 }
