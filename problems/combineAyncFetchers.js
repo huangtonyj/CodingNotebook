c2d4c198264328a9ec52a390a7cf8bab21ffaa71
tonyjhuang <huangtonyj91@gmail.com>
2019-02-07 15:18:02 -0800

    code combineAsyncFetchers and add Promise.all/race comparison examples

--- problems/combineAyncFetchers.js
+++ problems/combineAyncFetchers.js
@@ -1,8 +1,9 @@
-
+// Helper to generate random lag between 0 and 1000ms
 const randomLag = (magnitude = 1000) => {
   return Math.random() * magnitude;
 };
 
+// Function to generate a fetcher
 const fetcher = (queryStr) => {
   return new Promise(function (resolve, reject) {
     setTimeout(function () {
@@ -11,24 +12,37 @@ const fetcher = (queryStr) => {
   });
 };
 
+// Create a couple fetchers 
 const fetcherA = (queryStr) => fetcher(('A: ' + queryStr));
 const fetcherB = (queryStr) => fetcher(('B: ' + queryStr));
 const fetcherC = (queryStr) => fetcher(('C: ' + queryStr));
 
-const combineAsyncFetchers = (queryStr, fetchers, cb) => {
+// Custom function that mimics Promise.all desired resolved fetchers
+function combineAsyncFetchers(queryStr, fetchers, cb) {
   const responses = [];
-  let pendingFetchersCount = fetchers.length;
+  let pendingFetchersCount = 0;
 
   fetchers.forEach((iFetcher) => {
     iFetcher(queryStr).then(res => {
       responses.push(res);
-      pendingFetchersCount--;
-      if (pendingFetchersCount === 0) { 
+      pendingFetchersCount++;
+      if (pendingFetchersCount === fetchers.length) { // Can be tweak to return when n fetchers resolve async.
         responses.forEach(response => cb(response));
       }
     });
   });
-};
+}
 
+// combineAsyncFetchers example
 const fetchers = [fetcherA, fetcherB, fetcherC];
-combineAsyncFetchers('hello', fetchers, (res) => console.log(res));
+combineAsyncFetchers('hello from combineAsyncFetchers', fetchers, (res) => console.log(res));
+
+// Promise.all example
+const fetchersPromiseAll = [fetcherA('hello from Promise.all'), fetcherB('hello from Promise.all'), fetcherC('hello from Promise.all')];
+Promise.all(fetchersPromiseAll).then((responses) => {
+  responses.forEach(response => console.log(response));
+});
+
+// Promise.race example
+const fetchersPromiseRace = [fetcherA('hello from Promise.race'), fetcherB('hello from Promise.race'), fetcherC('hello from Promise.race')];
+Promise.race(fetchersPromiseRace).then((response) => console.log(response));
