d1f947e613af95f951a1af63dc0235c82a43cb69
tonyjhuang <huangtonyj91@gmail.com>
2019-02-07 14:43:05 -0800

    code combineAsyncFetchers

--- problems/combineAyncFetchers.js
+++ problems/combineAyncFetchers.js
@@ -15,31 +15,20 @@ const fetcherA = (queryStr) => fetcher(('A: ' + queryStr));
 const fetcherB = (queryStr) => fetcher(('B: ' + queryStr));
 const fetcherC = (queryStr) => fetcher(('C: ' + queryStr));
 
-const combineAsyncFetchers = (queryStr, fetchers) => {
+const combineAsyncFetchers = (queryStr, fetchers, cb) => {
+  const responses = [];
+  let pendingFetchersCount = fetchers.length;
+
   fetchers.forEach((iFetcher) => {
-    iFetcher('asdf').then((res)=> console.log(res));
+    iFetcher(queryStr).then(res => {
+      responses.push(res);
+      pendingFetchersCount--;
+      if (pendingFetchersCount === 0) { 
+        responses.forEach(response => cb(response));
+      }
+    });
   });
 };
 
-combineAsyncFetchers('hello', [fetcherA, fetcherB, fetcherC]);
-
-
-
-// function combineFetchers(fetchers) {
-//   const cbResult = [];
-//   let pendingCount = fetchers.length;
-//   return (str, cb) => {
-//       fetchers.forEach(fetcher => fetcher(str, (res, err) => {
-//             cbResult.push(res)) pendingCount--;
-//           if (pendingCount === 0) {
-//             cb(cbResult)
-//           }
-//         }
-//       }
-
-
-//       // errored callback
-//       // callbacks from different fetchers
-
-//     }
-// }
+const fetchers = [fetcherA, fetcherB, fetcherC];
+combineAsyncFetchers('hello', fetchers, (res) => console.log(res));
