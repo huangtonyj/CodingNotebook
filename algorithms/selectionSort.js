3e794142ac8c7375ff60fbe9298f0ae5ffda4eb1
tonyjhuang <huangtonyj91@gmail.com>
2019-02-06 17:56:50 -0800

    code selectionSort

--- algorithms/selectionSort.js
+++ algorithms/selectionSort.js
@@ -1,8 +1,22 @@
 function selectionSort(arr, sortFn) {
   sortFn = sortFn || ((a, b) => a - b);
 
-  
+  for (let i = 0; i < arr.length; i++) {
+    let currentMinIdx = i;
+    // Look for next min
+    for (let j = i + 1; j < arr.length; j++) {
+      if (sortFn(arr[currentMinIdx], arr[j]) > 0) {
+        currentMinIdx = j;
+      } 
+    }
 
+    // Swap places
+    const temp = arr[i];
+    arr[i] = arr[currentMinIdx];
+    arr[currentMinIdx] = temp;
+  }
+
+  return arr;
 }
 
 module.exports = selectionSort;
