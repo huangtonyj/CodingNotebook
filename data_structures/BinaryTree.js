dbb35cc5e32fb9043c90363c1296a26a9d62f317
tonyjhuang <huangtonyj91@gmail.com>
2019-02-08 12:13:21 -0800

    bug fix: size not height

--- data_structures/BinaryTree.js
+++ data_structures/BinaryTree.js
@@ -5,17 +5,24 @@ class BinaryTree {
     this.right = null;
   }
 
-  height() {
-    const left = this.left ? this.left.height() : 0;
-    const right = this.right ? this.right.height() : 0;
+  size() {
+    const left = this.left ? this.left.size() : 0;
+    const right = this.right ? this.right.size() : 0;
 
     return 1 + left + right;
   }
+
+
   
 }
 
 const myBinaryTree = new BinaryTree(1);
-
+  myBinaryTree.insert(2);
+  myBinaryTree.insert(3);
+  myBinaryTree.insert(4);
+  myBinaryTree.insert(5);
+  myBinaryTree.insert(6);
+  myBinaryTree.insert(7);
 
 console.log(myBinaryTree);
-console.log(myBinaryTree.height());
+console.log(myBinaryTree.size());
