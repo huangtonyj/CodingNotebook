e0d6f054a111d83e8bae45236102e1aba503d23e
tonyjhuang <huangtonyj91@gmail.com>
2019-02-08 12:26:37 -0800

    code BT contains method

--- data_structures/BinaryTree.js
+++ data_structures/BinaryTree.js
@@ -12,7 +12,38 @@ class BinaryTree {
     return 1 + left + right;
   }
 
+  height() {
+    const left = this.left ? this.left.height() : 0;
+    const right = this.right ? this.right.height() : 0;
 
+    return 1 + Math.max(left,right);
+  }
+
+  insert(value) {
+    // method to create a left-first full binary tree;
+
+    if (!this.left) { 
+      this.left = new BinaryTree(value); 
+    } else if (!this.right) { 
+      this.right = new BinaryTree(value); 
+    // } else if (this.left.height() === this.right.height() && this.left.size() === this.right.size()) {
+    } else if (this.left.size() === this.right.size()) {
+      this.left.insert(value);
+    } else {
+      this.right.insert(value);
+    }
+
+    return this;
+  }
+
+  contains(value) {
+    if (this.value === value) { return this; }
+
+    const left = this.left ? this.left.contains(value) : null;
+    const right = this.right ? this.right.contains(value) : null;
+
+    return left || right || null;
+  }
   
 }
 
@@ -21,8 +52,18 @@ const myBinaryTree = new BinaryTree(1);
   myBinaryTree.insert(3);
   myBinaryTree.insert(4);
   myBinaryTree.insert(5);
-  myBinaryTree.insert(6);
-  myBinaryTree.insert(7);
+  // myBinaryTree.insert(6);
+  // myBinaryTree.insert(7);
 
-console.log(myBinaryTree);
-console.log(myBinaryTree.size());
+
+// revisit insert method! logic is not right.
+
+
+// console.log(myBinaryTree);
+// console.log(myBinaryTree.size());
+// console.log(myBinaryTree.height());
+// console.log(myBinaryTree.left.height());
+// console.log(myBinaryTree.right.height());
+
+console.log(myBinaryTree.contains(3));
+
