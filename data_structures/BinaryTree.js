d11cb02324a547d5b49e1255284511270befe71b
tonyjhuang <huangtonyj91@gmail.com>
2019-02-08 12:06:02 -0800

    Binary Tree skeleton

---
+++ data_structures/BinaryTree.js
@@ -0,0 +1,16 @@
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+
+  
+}
+
+const myBinaryTree = new BinaryTree(1);
+
+
+console.log(myBinaryTree);
+console.log(myBinaryTree.height());
