1df6ac238e515203863cbbf020ba3acb6e062fed
tonyjhuang <huangtonyj91@gmail.com>
2019-02-11 08:14:57 -1000

    LRUCache skeleton

--- data_structures/LRUCache.js
+++ data_structures/LRUCache.js
@@ -1,10 +1,22 @@
+const NodeLLDouble = require('../data_structures/NodeLLDouble');
+
 class LRUCache {
-  constructor(size = 10) {
-    this.size = size;
+  constructor(maxSize = 1) {
+    this.maxSize = maxSize;
+    this.size = 0;
     this.cache = {};
+    this.LRU = new NodeLLDouble();
   }
 
-  get(value) {
+  insertKeyValuePair(key, value) {
+    // Write your code here.
+  }
 
+  getValueFromKey(key) {
+    // Write your code here.
+  }
+
+  getMostRecentKey() {
+    // Write your code here.
   }
 }
