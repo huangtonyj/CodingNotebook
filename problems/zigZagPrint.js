d12207ae14bef734e8cc727b541375b1cf47a784
tonyjhuang <huangtonyj91@gmail.com>
2019-02-11 16:48:59 -1000

    code zigZagPrint

--- problems/zigZagPrint.js
+++ problems/zigZagPrint.js
@@ -1,8 +1,29 @@
 function zigZagPrint(str, k) {
 
+  let result = [];
+  while(result.push([]) < k);
+  
+  let direction = -1;
+  let row = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    result.forEach((resultRow, resultRowIdx) => {
+      if (resultRowIdx !== row) {
+        resultRow.push(' ');
+      } else {
+        resultRow.push(str[i]);
+      }
+    });
+
+
+    if (row === 0 || row === k - 1) { direction *= -1; }
+    row += direction;
+  }
+
+  result.forEach(thisRow => console.log(thisRow.join("")));
 }
 
-console.log(zigZagPrint('thisisazigzag', 4));
+zigZagPrint('thisisazigzag', 4);
 
 // t     a     g
 //  h   s z   a
