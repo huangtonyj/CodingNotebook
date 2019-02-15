1743091dffe866317e30ae2f03e6f3a2659d982e
tonyjhuang <huangtonyj91@gmail.com>
2019-02-15 11:00:49 -0800

    code helper isCompleteTree()

--- data_structures/BinaryTree.js
+++ data_structures/BinaryTree.js
@@ -22,16 +22,18 @@ class BinaryTree {
   insert(value) {
     // method to create a left-first full binary tree;
 
-    if (!this.left) { 
-      this.left = new BinaryTree(value); 
-    } else if (!this.right) { 
-      this.right = new BinaryTree(value); 
+    // if (!this.left) { 
+    //   this.left = new BinaryTree(value); 
+    // } else if (!this.right) { 
+    //   this.right = new BinaryTree(value); 
     // } else if (this.left.height() === this.right.height() && this.left.size() === this.right.size()) {
-    } else if (this.left.size() === this.right.size()) {
-      this.left.insert(value);
-    } else {
-      this.right.insert(value);
-    }
+    // } else if (this.left.height() === this.right.height()) {
+    //   this.left.insert(value);
+    // } else {
+    //   this.right.insert(value);
+    // }
+
+   
 
     return this;
   }
@@ -45,6 +47,17 @@ class BinaryTree {
     return left || right || null;
   }
   
+  isCompleteTree() {
+    let left = this.left, right = this.right;
+
+    while (left && right) {
+      left = left.left;
+      right = right.right;
+    }
+    
+    return left === null && right === null;
+  }
+
 }
 
 const myBinaryTree = new BinaryTree(1);
@@ -52,18 +65,18 @@ const myBinaryTree = new BinaryTree(1);
   myBinaryTree.insert(3);
   myBinaryTree.insert(4);
   myBinaryTree.insert(5);
-  // myBinaryTree.insert(6);
-  // myBinaryTree.insert(7);
+  myBinaryTree.insert(6);
+  myBinaryTree.insert(7);
 
 
 // revisit insert method! logic is not right.
 
 
-// console.log(myBinaryTree);
+console.log(myBinaryTree);
 // console.log(myBinaryTree.size());
 // console.log(myBinaryTree.height());
 // console.log(myBinaryTree.left.height());
 // console.log(myBinaryTree.right.height());
 
-console.log(myBinaryTree.contains(3));
+// console.log(myBinaryTree.contains(3));
 
