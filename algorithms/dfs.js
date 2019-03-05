23d54bbc70f8b228c6bcc6559775c68638d05bfc
tonyjhuang <huangtonyj91@gmail.com>
2019-03-05 14:02:42 -0800

    code dfsPreOrderIterative()

--- algorithms/dfs.js
+++ algorithms/dfs.js
@@ -20,6 +20,19 @@ function dfsPostOrder (node) {
   return dfsPostOrder(node.left).concat(dfsPostOrder(node.right), node.value);
 }
 
+
+function dfsPreOrderIterative (node) {
+  const result = [];
+  const stack = [node];
+  while (stack.length) {
+    const currentNode = stack.pop();
+    result.push(currentNode.value);
+    if (currentNode.right) { stack.push(currentNode.right); }
+    if (currentNode.left) { stack.push(currentNode.left); }
+  }
+  return result;
+}
+
 const rootNode = new NodeBT(4);
   rootNode.left = new NodeBT(2);
     rootNode.left.left = new NodeBT(1);
@@ -37,3 +50,8 @@ console.log('dfsInOrder:', dfsInOrder(rootNode), [1, 2, 3, 4, 5, 6, 7]);
 console.log('dfsReverseOrder:', dfsReverseOrder(rootNode), [7, 6, 5, 4, 3, 2, 1]);
 console.log('dfsPreOrder:', dfsPreOrder(rootNode), [4, 2, 1, 3, 6, 5, 7]);
 console.log('dfsPostOrder:', dfsPostOrder(rootNode), [1, 3, 2, 5, 7, 6, 4]);
+
+// console.log('dfsInOrderIterative:', dfsInOrderIterative(rootNode), [1, 2, 3, 4, 5, 6, 7]);
+// console.log('dfsReverseOrderIterative:', dfsReverseOrderIterative(rootNode), [7, 6, 5, 4, 3, 2, 1]);
+console.log('dfsPreOrderIterative:', dfsPreOrderIterative(rootNode), [4, 2, 1, 3, 6, 5, 7]);
+// console.log('dfsPostOrderIterative:', dfsPostOrderIterative(rootNode), [1, 3, 2, 5, 7, 6, 4]);
