b6a2a06d711e32ebfd2c60535b0f35612c2e10b9
tonyjhuang <huangtonyj91@gmail.com>
2019-03-06 15:42:12 -0800

    code removeNonFullBT

--- problems/removeNonFullBT.js
+++ problems/removeNonFullBT.js
@@ -1,8 +1,28 @@
 const NodeBT = require('../data_structures/NodeBT');
 
 function removeNonFullBT(node) {
+  if (!node) { return; }
 
+  while (node.left && isNotFullNode(node.left)) {
+    node.left = node.left.left || node.left.right;
+  }
+
+  while (node.right && isNotFullNode(node.right)) {
+    node.right = node.right.left || node.right.right;
+  }
+
+  if (node.left) removeNonFullBT(node.left);
+  if (node.right) removeNonFullBT(node.right);
+
+  return node;
 }
+
+const isNotFullNode = (node) => {
+  const left = node.left ? 1 : 0;
+  const right = node.right ? 1 : 0;
+
+  return (left + right) === 1;
+};
 
 // Given: 
 //          0
@@ -19,7 +39,6 @@ function removeNonFullBT(node) {
 //         /   \
 //        6     7
 
-
 const myBT = new NodeBT(0);
   myBT.left = new NodeBT(1);
   myBT.left.left = new NodeBT(3);
@@ -30,3 +49,9 @@ const myBT = new NodeBT(0);
   myBT.right.right.right = new NodeBT(7);
 
 console.log(removeNonFullBT(myBT));
+// console.log(isNotFullNode(myBT), true);
+// console.log(isNotFullNode(myBT.left), false);
+// console.log(isNotFullNode(myBT.right), false);
+// console.log(isNotFullNode(myBT.right.right), true);
+// console.log(isNotFullNode(myBT.right.right.left), true);
+// console.log(isNotFullNode(myBT.right.right.right), true);
