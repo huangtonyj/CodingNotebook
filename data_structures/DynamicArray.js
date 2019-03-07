69c0c6825cc899cc88c911b80add4ff0a7a2e3fe
tonyjhuang <huangtonyj91@gmail.com>
2019-03-07 14:29:14 -0800

    code DynamicArray class

--- data_structures/DynamicArray.js
+++ data_structures/DynamicArray.js
@@ -18,28 +18,54 @@ class DynamicArray {
 
   push(val) {
     if (this.length === this.capacity) this._resize();
-    this.store[this.length - 1] = val;
+    
+    this.store.set(this.length - 1, val);
     this.length++;
+    return this;
   }
-
+  
   pop() {
     if (this.length === 0) throw 'Out of range';
     
-    const val = this.store[this.length - 1];
-    this.store[this.length - 1] = null;
+    const val = this.store.get(this.length - 1);
+    this.store.set(this.length - 1, null);
+    this.length--;
     return val;
   }
   
   unshift(val) {
+    if (this.length === this.capacity) this._resize();
     
+    for (let i = this.length; i > 0; i--) {
+      this.store.set(i, this.store.get(i - 1));
+    }
+    
+    this.store.set(0, val);
+    this.length++;
+    
+    return this;
   }
   
   shift() {
+    if (this.length === 0) throw 'Out of range';
+    const val = this.store.get(0);
 
+    for (let i = 0; i < this.length - 1; i++) {
+      this.store.set(i, this.store.get(i + 1));
+    }
+
+    this.length--;
+    
+    return val;
   }
 
   _resize() {
-
+    const newStore = new StaticArray(this.capacity * 2);
+    for (let i = 0; i < this.length; i++) {
+      newStore.set(i, this.store.get(i));
+    }
+    this.capacity *= 2;
+    this.store = newStore;
   }
 }
 
