cb558b8e0b7232600bd2c0dc410b4b85eb40c4e5
tonyjhuang <huangtonyj91@gmail.com>
2019-03-06 21:19:11 -0800

    code quickSort2(), iterative version

--- algorithms/quickSort.js
+++ algorithms/quickSort.js
@@ -1,3 +1,4 @@
+// RECURSIVE quickSort
 function quickSort(arr, sortFn) {
   if (arr.length < 2) { return arr; }
   
@@ -13,6 +14,33 @@ function quickSort(arr, sortFn) {
   return quickSort(left, sortFn).concat([pivot], quickSort(right, sortFn));
 }
 
+// ITERATIVE quickSort
+function quickSort2(arr, sortFn) {
+  sortFn = sortFn || ((a, b) => a - b);
+  let result = [];
+  let stack = [arr];
+
+  while (stack.length) {
+    const currentArr = stack.pop();
+    
+    if (currentArr.length < 2) { 
+      result = result.concat(currentArr); 
+      continue;
+    }
+
+    const pivot = currentArr.shift();
+    const left = [], right = [];
+
+    currentArr.forEach(el => {
+      (sortFn(el, pivot) < 0) ? left.push(el): right.push(el);
+    });
+
+    stack.push(right, [pivot], left);
+  }
+
+  return result;
+}
+
 module.exports = quickSort;
 
 // console.log(quickSort([3, 4, 1, 5, 2, 1]), [1, 1, 2, 3, 4, 5]);
@@ -21,3 +49,11 @@ module.exports = quickSort;
 
 // console.log(quickSort([3, 4, 1, 5, 2], (a, b) => b - a), [5, 4, 3, 2, 1]);
 // console.log(quickSort([3, 4, 1, 5, 2, 6], (a, b) => b - a), [6, 5, 4, 3, 2, 1]);
+
+
+// console.log(quickSort2([3, 4, 1, 5, 2, 1]), [1, 1, 2, 3, 4, 5]);
+// console.log(quickSort2([3, 4, 1, 5, 2]), [1, 2, 3, 4, 5]);
+// console.log(quickSort2([3, 4, 1, 5, 2, 6]), [1, 2, 3, 4, 5, 6]);
+
+// console.log(quickSort2([3, 4, 1, 5, 2], (a, b) => b - a), [5, 4, 3, 2, 1]);
+// console.log(quickSort2([3, 4, 1, 5, 2, 6], (a, b) => b - a), [6, 5, 4, 3, 2, 1]);
