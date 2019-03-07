5c44fac249769d6cf227a1297d930b541d590472
tonyjhuang <huangtonyj91@gmail.com>
2019-03-07 11:25:57 -0800

    Dynamic Array DS Class Skelton

---
+++ data_structures/DynamicArray.js
@@ -0,0 +1,37 @@
+const StaticArray = require('./StaticArray');
+
+class DynamicArray {
+  constructor() {
+    this.store = new StaticArray(8);
+    this.capacity = 8;
+    this.lenght = 0;
+  }
+
+  get(idx) {
+    
+  }
+
+  set(idx, val) {
+
+  }
+
+  pop() {
+
+  }
+
+  push() {
+
+  }
+
+  shift() {
+
+  }
+
+  unshift() {
+
+  }
+
+  resize() {
+    
+  }
+}
