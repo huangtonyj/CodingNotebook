0aea8d813825b268d7741abf5408b304a28c3465
tonyjhuang <huangtonyj91@gmail.com>
2019-03-07 14:05:08 -0800

    code push() and pop()

--- data_structures/DynamicArray.js
+++ data_structures/DynamicArray.js
@@ -4,34 +4,43 @@ class DynamicArray {
   constructor() {
     this.store = new StaticArray(8);
     this.capacity = 8;
-    this.lenght = 0;
+    this.length = 0;
   }
 
   get(idx) {
-    
+    return this.store.get(idx);
   }
 
   set(idx, val) {
+    return this.store.set(idx, val);
+    // if idx is larger than length, previous values = null;
+  }
 
+  push(val) {
+    if (this.length === this.capacity) this._resize();
+    this.store[this.length - 1] = val;
+    this.length++;
   }
 
   pop() {
-
+    if (this.length === 0) throw 'Out of range';
+    
+    const val = this.store[this.length - 1];
+    this.store[this.length - 1] = null;
+    return val;
   }
-
-  push() {
-
+  
+  unshift(val) {
+    
   }
-
+  
   shift() {
 
   }
 
-  unshift() {
+  _resize() {
 
-  }
-
-  resize() {
-    
   }
 }
+
+module.exports = DynamicArray;
