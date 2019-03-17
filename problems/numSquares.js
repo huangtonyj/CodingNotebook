e2b4252dc9cf45bf37873669852cdd30d1ac7531
tonyjhuang <huangtonyj91@gmail.com>
2019-03-16 19:34:40 -0700

    draft numSquares, not correct

---
+++ problems/numSquares.js
@@ -0,0 +1,15 @@
+function numSquares(n) {
+
+  const result = [];
+
+  while (n !== 0) {
+    const current = Math.floor(Math.sqrt(n));
+    result.push(current);
+    n -= current * current;
+  }
+
+  return result;
+}
+
+console.log(numSquares(13), [3, 2]);
+console.log(numSquares(12), [3, 2]);
