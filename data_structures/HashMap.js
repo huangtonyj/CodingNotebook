548934ee57f94a74cbe47f792e310d84499c514e
tonyjhuang <huangtonyj91@gmail.com>
2019-10-09 23:04:47 -0700

    Skeleton for HashMap class

---
+++ data_structures/HashMap.js
@@ -0,0 +1,29 @@
+// Dynamic resizing array
+// Hash function
+// LL
+
+class HashMap {
+  constructor() {
+
+  }
+
+  get(key) {
+
+  }
+
+  set(key, value) {
+    
+  }
+
+  keys() {
+
+  }
+
+  values() {
+
+  }
+
+  keyValues() {
+
+  }
+}
