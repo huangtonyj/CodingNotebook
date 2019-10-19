3d1630a26ae86ddd17d6e8a1209bb58d81be7168
tonyjhuang <huangtonyj91@gmail.com>
2019-10-19 11:23:30 -0700

    Migrate tests to selectionSort.test.js

--- algorithms/selectionSort.js
+++ algorithms/selectionSort.js
@@ -20,9 +20,3 @@ function selectionSort(arr, sortFn) {
 }
 
 module.exports = selectionSort;
-
-console.log(selectionSort([3, 4, 1, 5, 2]), [1, 2, 3, 4, 5]);
-console.log(selectionSort([3, 4, 1, 5, 2, 6]), [1, 2, 3, 4, 5, 6]);
-
-console.log(selectionSort([3, 4, 1, 5, 2], (a, b) => b - a), [5, 4, 3, 2, 1]);
-console.log(selectionSort([3, 4, 1, 5, 2, 6], (a, b) => b - a), [6, 5, 4, 3, 2, 1]);
