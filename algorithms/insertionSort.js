2c322f5f0a988e42302eb6bc41b1f2d979a839cb
tonyjhuang <huangtonyj91@gmail.com>
2019-10-19 11:21:11 -0700

    module.exports = insertionSort;

--- algorithms/insertionSort.js
+++ algorithms/insertionSort.js
@@ -15,7 +15,4 @@ function insertionSort(arr, sortFn) {
   return arr;
 }
 
-console.log(insertionSort([3,2,1,4]), [1,2,3,4]);
-console.log(insertionSort([3,2,1,4], (a,b) => b-a), [4,3,2,1]);
-console.log(insertionSort([3,2,5,1,4]), [1,2,3,4,5]);
-console.log(insertionSort([3,2,5,1,4], (a,b) => b-a), [5,4,3,2,1]);
+module.exports = insertionSort;
