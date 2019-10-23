0912ccb84c1cf28767bdefe012feeebe9283a727
tonyjhuang <huangtonyj91@gmail.com>
2019-10-22 22:23:53 -0500

    caesarCipherEncryptor() code

---
+++ problems/caesarCipherEncryptor.js
@@ -0,0 +1,16 @@
+function caesarCipherEncryptor(str, key) {
+  const alphabetIdx = (char) => { return char.charCodeAt() - 97; };
+  const shiftKey = (num) => { return (num + key) % 26; };
+  const encode = (num) => { return String.fromCharCode(num + 97); };
+  
+  return Array.from(str)
+    .map((char) => alphabetIdx(char))
+    .map((num) => shiftKey(num))
+    .map((num) => encode(num))
+    .join("");
+}
+
+module.exports = caesarCipherEncryptor;
+
+console.log(caesarCipherEncryptor("abcxyz", 0));
+console.log(caesarCipherEncryptor("abcxyz", 1));
