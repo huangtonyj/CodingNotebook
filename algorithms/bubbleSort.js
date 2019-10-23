7cf1e47dc0bf0579e7ef5026ba374783f0e0079b
tonyjhuang <huangtonyj91@gmail.com>
2019-10-22 22:13:00 -0500

    bubbleSort() refactor

--- algorithms/bubbleSort.js
+++ algorithms/bubbleSort.js
@@ -7,16 +7,14 @@ function bubbleSort(arr, sortFn) {
 
   const sortASC = (a,b) => a - b;
   sortFn = sortFn || sortASC;
-  // let temp;
 
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - 1; j++) {
-      // const currentEl = arr[j];
-      // const nextEl = arr[j + 1];
-      if (sortFn(arr[j], arr[j + 1]) > 0) {
-        const temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = temp;
+      const currentEl = arr[j];
+      const nextEl = arr[j + 1];
+      
+      if (sortFn(currentEl, nextEl) > 0) {
+        swap(j, j + 1);
       }
     }
   }
