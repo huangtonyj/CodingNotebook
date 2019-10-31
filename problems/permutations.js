3ff3cff2ab50c5b842db209e626b28b927ecc888
tonyjhuang <huangtonyj91@gmail.com>
2019-10-31 00:35:53 -0700

    permutations()

--- problems/permutations.js
+++ problems/permutations.js
@@ -2,10 +2,9 @@ function permutations(arr) {
   if (arr.length < 2) { return arr; }
 
   const result = [];
-  let perms;
-
+  
   for (let i = 0 ; i < arr.length ; i++) {
-    perms = permutations(arr.slice(0, i).concat(arr.slice(i + 1, arr.length)));
+    const perms = permutations(arr.slice(0, i).concat(arr.slice(i + 1, arr.length)));
    
     perms.forEach((perm) => {
       result.push([arr[i]].concat(perm));
@@ -15,4 +14,4 @@ function permutations(arr) {
   return result;
 }
 
-console.log(permutations([1, 2, 3, 4]));
+module.exports = permutations;
