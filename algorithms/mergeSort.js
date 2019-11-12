84631c346544f0d1d00234c50f4b0d3249cacdba
tonyjhuang <huangtonyj91@gmail.com>
2019-11-12 01:09:54 -0800

    mergeSort cleanup

--- algorithms/mergeSort.js
+++ algorithms/mergeSort.js
@@ -20,8 +20,3 @@ function mergeSort(arr, sortFn) {
 
 module.exports = mergeSort;
 
-console.log(mergeSort([3, 4, 1, 5, 2]), [1, 2, 3, 4, 5]);
-console.log(mergeSort([3, 4, 1, 5, 2, 6]), [1, 2, 3, 4, 5, 6]);
-
-console.log(mergeSort([3, 4, 1, 5, 2], (a, b) => b - a), [5, 4, 3, 2, 1]);
-console.log(mergeSort([3, 4, 1, 5, 2, 6], (a, b) => b - a), [6, 5, 4, 3, 2, 1]);
