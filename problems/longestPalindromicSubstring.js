94240ea9ef148255245e1258523a4c35060c6f9f
tonyjhuang <huangtonyj91@gmail.com>
2019-11-12 00:55:35 -0800

    longestPalindromicSubstring() refactor

--- problems/longestPalindromicSubstring.js
+++ problems/longestPalindromicSubstring.js
@@ -1,35 +1,17 @@
+// Init two pointers at index 0.
+// Inch one pointer zero to one step ahead.
+// If the value at the two pointers are the same, 
+// expand from the pointers indices and get current palindromic substring;
+// then compare current to longest.
+
 function longestPalindromicSubstring(str) {
-  
-  let current = '';
+  let result = '';
   let i = 0, j = 0;
 
   while (i < str.length) {
-
-    const iLetter = str[i];
-    const jLetter = str[j];
-
-    if (iLetter === jLetter) {
-      let leftPointer = j;
-      let rightPointer = i;
-      // console.log(leftPointer, rightPointer);
-
-      while (leftPointer > -1 && rightPointer < str.length) {
-        if (str[leftPointer] === str[rightPointer]) {
-          leftPointer--;
-          rightPointer++;
-        } else {
-          break;
-        }
-        // console.log(leftPointer, rightPointer);
-      }
-
-      const currentPalindromeSubString = str.slice(leftPointer + 1, rightPointer);
-      // console.log(currentPalindromeSubString);
-
-      if (currentPalindromeSubString.length > current.length) {
-        current = currentPalindromeSubString;
-      }
-
+    if (str[i] === str[j]) {
+      const current = getPalindromicSubstr(str, j, i);
+      if (current.length > result.length) { result = current; }
     }
 
     if (j === i) {
@@ -37,14 +19,19 @@ function longestPalindromicSubstring(str) {
     } else {
       j++;
     }
-    // console.log(i);
-
   }
 
-  return current;
+  return result;
+}
+
+function getPalindromicSubstr(str, leftPointer, rightPointer) {
+  while (leftPointer > -1 && rightPointer < str.length) {
+    if (str[leftPointer] !== str[rightPointer]) { break; }
+    leftPointer--;
+    rightPointer++;
+  }
+
+  return str.slice(leftPointer + 1, rightPointer);
 }
 
 module.exports = longestPalindromicSubstring;
-
-
-console.log(longestPalindromicSubstring('anoona'));
