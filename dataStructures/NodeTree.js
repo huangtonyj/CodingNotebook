6814242ca8b71daf3ec93c07f04f89e8145ae62e
tonyjhuang <huangtonyj91@gmail.com>
2019-11-11 23:18:47 -0800

    NodeTree.breadthFirstSearch()

--- dataStructures/NodeTree.js
+++ dataStructures/NodeTree.js
@@ -3,6 +3,25 @@ class NodeTree {
     this.value = value;
     this.children = [];
   }
+
+  addChild(value) {
+    this.children.push(new NodeTree(value));
+    return this;
+  }
+
+  breadthFirstSearch() {
+    const result = [];
+    let queue = [this];
+
+    while (queue.length) {
+      const currentNode = queue.shift();
+      result.push(currentNode.value);
+      queue = queue.concat(currentNode.children);
+    }
+
+    return result;
+  }
+  
 }
 
 module.exports = NodeTree;
