fdffcfa02d88cf78f31724ea54452185127a6730
tonyjhuang <huangtonyj91@gmail.com>
2019-11-11 20:24:47 -0800

    refactor MinMaxStack

--- dataStructures/MinMaxStack.js
+++ dataStructures/MinMaxStack.js
@@ -19,42 +19,26 @@ class MinMaxStack {
     return this.stack.length === 0;
   }
   
-  min() {
+  getMin() {
     return this.isEmpty() ? null : this.minStack[this.minStack.length - 1];
   }
 
-  max() {
+  getMax() {
     return this.isEmpty() ? null : this.maxStack[this.maxStack.length - 1];
   }
   
   push(el) {
-    if (el <= this.min() || this.isEmpty()) { this.minStack.push(el); }
-    if (el >= this.max() || this.isEmpty()) { this.maxStack.push(el); }
+    if (el <= this.getMin() || this.isEmpty()) { this.minStack.push(el); }
+    if (el >= this.getMax() || this.isEmpty()) { this.maxStack.push(el); }
     this.stack.push(el);
-    // console.log(this);
   }
   
   pop() {
     let result = this.stack.pop();
-    if (result === this.min()) { this.minStack.pop(); }
-    if (result === this.max()) { this.maxStack.pop(); }
-    // console.log(this);
+    if (result === this.getMin()) { this.minStack.pop(); }
+    if (result === this.getMax()) { this.maxStack.pop(); }
     return result;
   }
-
 }
 
 module.exports = MinMaxStack;
-
-// const myMinMaxStack = new MinMaxStack();
-//   myMinMaxStack.push(1);
-//   myMinMaxStack.push(2);
-//   myMinMaxStack.push(3);
-//   myMinMaxStack.push(4);
-//   myMinMaxStack.push(3);
-//   myMinMaxStack.push(0);
-//   console.log(myMinMaxStack.pop());
-//   console.log(myMinMaxStack.pop());
-//   console.log(myMinMaxStack.pop());
-
-// const myMinMaxStack2 = new MinMaxStack([1, 0, 2, 5, 4, 8]);
