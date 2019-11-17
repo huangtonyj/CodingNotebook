4f773a7e2a97675e50fadf89e2e88107723ae8b0
tonyjhuang <huangtonyj91@gmail.com>
2019-11-17 09:25:46 -0800

    riverSizes Land class test

--- test/problems/riverSizes.test.js
+++ test/problems/riverSizes.test.js
@@ -1,4 +1,4 @@
-const riverSizes = require('../../problems/riverSizes');
+const {riverSizes, Land} = require('../../problems/riverSizes');
 
 describe('riverSizes()', () => {
   it('Test Case #1', () => {
@@ -143,6 +143,166 @@ describe('riverSizes()', () => {
     ];
     const expected = [1, 1, 2, 6, 10];
     expect(riverSizes(testInput).sort((a, b) => a - b)).toEqual(expected);
+  });
+
+});
+
+
+describe('Land.riverSizes()', () => {
+  it('Test Case #1', () => {
+    const testInput = [
+      [0]
+    ];
+    const expected = [];
+    const testLand = new Land(testInput);
+    expect(testLand.riverSizes().sort((a, b) => a - b)).toEqual(expected);
+  });
+
+  it('Test Case #2', () => {
+    const testInput = [
+      [1]
+    ];
+    const expected = [1];
+    const testLand = new Land(testInput);
+    expect(testLand.riverSizes().sort((a, b) => a - b)).toEqual(expected);
+  });
+
+  it('Test Case #3', () => {
+    const testInput = [
+      [1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0]
+    ];
+    const expected = [1, 2, 3];
+    const testLand = new Land(testInput);
+    expect(testLand.riverSizes().sort((a, b) => a - b)).toEqual(expected);
+  });
+
+  it('Test Case #4', () => {
+    const testInput = [
+      [1, 0, 0, 1],
+      [1, 0, 1, 0],
+      [0, 0, 1, 0],
+      [1, 0, 1, 0]
+    ];
+    const expected = [1, 1, 2, 3];
+    const testLand = new Land(testInput);
+    expect(testLand.riverSizes().sort((a, b) => a - b)).toEqual(expected);
+  });
+
+  it('Test Case #5', () => {
+    const testInput = [
+      [1, 0, 0, 1, 0],
+      [1, 0, 1, 0, 0],
+      [0, 0, 1, 0, 1],
+      [1, 0, 1, 0, 1],
+      [1, 0, 1, 1, 0],
+    ];
+    const expected = [1, 2, 2, 2, 5];
+    const testLand = new Land(testInput);
+    expect(testLand.riverSizes().sort((a, b) => a - b)).toEqual(expected);
+  });
+
+  it('Test Case #6', () => {
+    const testInput = [
+      [1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0],
+      [1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0],
+      [0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1],
+      [1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0],
+      [1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1],
+    ];
+    const expected = [1, 1, 2, 2, 5, 21];
+    const testLand = new Land(testInput);
+    expect(testLand.riverSizes().sort((a, b) => a - b)).toEqual(expected);
+  });
+
+  it('Test Case #7', () => {
+    const testInput = [
+      [1, 0, 0, 0, 0, 0, 1],
+      [0, 1, 0, 0, 0, 1, 0],
+      [0, 0, 1, 0, 1, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0],
+      [0, 0, 1, 0, 1, 0, 0],
+      [0, 1, 0, 0, 0, 1, 0],
+      [1, 0, 0, 0, 0, 0, 1],
+    ];
+    const expected = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    const testLand = new Land(testInput);
+    expect(testLand.riverSizes().sort((a, b) => a - b)).toEqual(expected);
+  });
+
+  it('Test Case #8', () => {
+    const testInput = [
+      [1, 0, 0, 0, 0, 0, 1],
+      [0, 1, 0, 0, 0, 1, 0],
+      [0, 0, 1, 0, 1, 0, 0],
+      [0, 0, 1, 1, 1, 0, 0],
+      [0, 0, 1, 0, 1, 0, 0],
+      [0, 1, 0, 0, 0, 1, 0],
+      [1, 0, 0, 0, 0, 0, 1],
+    ];
+    const expected = [1, 1, 1, 1, 1, 1, 1, 1, 7];
+    const testLand = new Land(testInput);
+    expect(testLand.riverSizes().sort((a, b) => a - b)).toEqual(expected);
+  });
+
+  it('Test Case #9', () => {
+    const testInput = [
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+    ];
+    const expected = [];
+    const testLand = new Land(testInput);
+    expect(testLand.riverSizes().sort((a, b) => a - b)).toEqual(expected);
+  });
+
+  it('Test Case #10', () => {
+    const testInput = [
+      [1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1],
+    ];
+    const expected = [49];
+    const testLand = new Land(testInput);
+    expect(testLand.riverSizes().sort((a, b) => a - b)).toEqual(expected);
+  });
+
+  it('Test Case #11', () => {
+    const testInput = [
+      [1, 1, 0, 0, 0, 0, 1, 1],
+      [1, 0, 1, 1, 1, 1, 0, 1],
+      [0, 1, 1, 0, 0, 0, 1, 1],
+    ];
+    const expected = [3, 5, 6];
+    const testLand = new Land(testInput);
+    expect(testLand.riverSizes().sort((a, b) => a - b)).toEqual(expected);
+  });
+
+  it('Test Case #12', () => {
+    const testInput = [
+      [1, 1, 0],
+      [1, 0, 1],
+      [1, 1, 1],
+      [1, 1, 0],
+      [1, 0, 1],
+      [0, 1, 0],
+      [1, 0, 0],
+      [1, 0, 0],
+      [0, 0, 0],
+      [1, 0, 0],
+      [1, 0, 1],
+      [1, 1, 1],
+    ];
+    const expected = [1, 1, 2, 6, 10];
+    const testLand = new Land(testInput);
+    expect(testLand.riverSizes().sort((a, b) => a - b)).toEqual(expected);
   });
 
 });
