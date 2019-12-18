99cde2b7be00c0f2cc34458f8ae55207bc61131c
tonyjhuang <huangtonyj91@gmail.com>
2019-12-17 23:30:35 -0800

    refactor longestContiguousSequence

--- problems/longestContiguousSequence.js
+++ problems/longestContiguousSequence.js
@@ -2,11 +2,11 @@
 // Write a function that takes two users' browsing histories as input and returns the longest contiguous sequence of URLs that appear in both.
 // For example, given the following two users' histories:
 
-const user1 = ['/home', '/register', '/login', '/user', '/one', '/two'];
-const user2 = ['/home', '/red', '/login', '/user', '/one', '/pink'];
+// const user1 = ['/home', '/register', '/login', '/user', '/one', '/two'];
+// const user2 = ['/home', '/red', '/login', '/user', '/one', '/pink'];
 
 // You should return the following:
-const expected = ['/login', '/user', '/one'];
+// const expected = ['/login', '/user', '/one'];
 
 const LinkListHash = require('../dataStructures/LinkListHash');
 
@@ -17,13 +17,12 @@ function longestContiguousSequence (str1, str2) {
 
   str2.forEach((el, idx) => {
     const elNode1 = linkListHash1.getNode(el);
+    const nextEl1 = elNode1 ? elNode1.next.value.val : null;
+    const nextEl2 = str2[idx + 1];
 
     current.push(el);
-    if (elNode1 && elNode1.prev && elNode1.next.value.val === str2[idx + 1]) {
-      // console.log('yes', el, elNode1);
-      // current.push(el);
-      
-    } else {
+    
+    if (nextEl1 !== nextEl2) {
       result = (current.length > result.length) ? current : result;
       current = [];
     }
@@ -32,4 +31,4 @@ function longestContiguousSequence (str1, str2) {
   return result;
 }
 
-console.log(longestContiguousSequence(user1, user2), expected);
+module.exports = longestContiguousSequence;
