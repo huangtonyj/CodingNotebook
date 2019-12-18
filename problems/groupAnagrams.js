2cc0659c964da822f20b28a4d402f658e5da07ad
tonyjhuang <huangtonyj91@gmail.com>
2019-12-17 22:07:29 -0800

    groupAnagrams()

---
+++ problems/groupAnagrams.js
@@ -0,0 +1,14 @@
+function groupAnagrams(arr) {
+  const groups = {};
+
+  arr.forEach((str) => {
+    const group = str.split('').sort().join('');
+
+    if (!groups[group]) { groups[group] = []; }
+    groups[group].push(str);
+  });
+
+  return Object.values(groups);
+}
+
+module.exports = groupAnagrams;
