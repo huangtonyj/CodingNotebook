637aa5160932507720587ad8ae4d02a26f5416ab
tonyjhuang <huangtonyj91@gmail.com>
2019-12-18 23:14:08 +0800

    module.exports = bfs;

--- algorithms/bfs.js
+++ algorithms/bfs.js
@@ -17,19 +17,21 @@ function bfs(node) {
   return result;
 }
 
-const rootNode = new NodeBT(4);
-  rootNode.left = new NodeBT(2);
-    rootNode.left.left = new NodeBT(1);
-    rootNode.left.right = new NodeBT(3);
-  rootNode.right = new NodeBT(6);
-    rootNode.right.left = new NodeBT(5);
-    rootNode.right.right = new NodeBT(7);
+module.exports = bfs;
+
+// const rootNode = new NodeBT(4);
+//   rootNode.left = new NodeBT(2);
+//     rootNode.left.left = new NodeBT(1);
+//     rootNode.left.right = new NodeBT(3);
+//   rootNode.right = new NodeBT(6);
+//     rootNode.right.left = new NodeBT(5);
+//     rootNode.right.right = new NodeBT(7);
 //      4
 //    /   \
 //   2     6
 //  / \   / \
 // 1   3 5   7
 
-console.log(bfs(rootNode), [ 4, 2, 6, 1, 3, 5, 7]);
+// console.log(bfs(rootNode), [ 4, 2, 6, 1, 3, 5, 7]);
 
 
