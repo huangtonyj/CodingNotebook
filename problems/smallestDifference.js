6243990f04f64a106cc9377a6ea1ec4a4847aea0
tonyjhuang <huangtonyj91@gmail.com>
2019-12-17 22:47:17 -0800

    Rename variables

--- problems/smallestDifference.js
+++ problems/smallestDifference.js
@@ -5,29 +5,29 @@ function smallestDifference(arr1, arr2) {
   arr1 = quickSort(arr1);
   arr2 = quickSort(arr2);
 
-  let currentIdx1 = 0;
-  let currentIdx2 = 0;
-  let currentSmallestDifference = Infinity;
-  let result;
+  let idx1 = 0;
+  let idx2 = 0;
+  let currentSmallestDiff = Infinity;
+  let smallestPair;
 
-  while (currentIdx1 < arr1.length && currentIdx2 < arr2.length) {
-    const currentValue1 = arr1[currentIdx1];
-    const currentValue2 = arr2[currentIdx2];
-    const currentDifference = Math.abs(currentValue1 - currentValue2);
+  while (idx1 < arr1.length && idx2 < arr2.length) {
+    const num1 = arr1[idx1];
+    const num2 = arr2[idx2];
+    const currentDiff = Math.abs(num1 - num2);
 
-    if (currentDifference < currentSmallestDifference) {
-      currentSmallestDifference = currentDifference;
-      result = [currentValue1, currentValue2];
+    if (currentDiff < currentSmallestDiff) {
+      currentSmallestDiff = currentDiff;
+      smallestPair = [num1, num2];
     }
 
-    if (currentValue1 > currentValue2) {
-      currentIdx2++;
+    if (num1 > num2) {
+      idx2++;
     } else {
-      currentIdx1++;
+      idx1++;
     }
   }
 
-  return result;
+  return smallestPair;
 }
 
 module.exports = smallestDifference;
