18b5c8aaae1ecc16ce354bc50f658648600ed7f1
tonyjhuang <huangtonyj91@gmail.com>
2019-12-17 20:10:27 -0800

    Use BinaryTree imported from ./dataStructures

--- problems/branchSum.js
+++ problems/branchSum.js
@@ -1,22 +1,10 @@
-// This is the class of the input root.
-// Do not edit it.
-class BinaryTree {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
-
 function branchSums(root, parent = 0) {
-  
   if (root && !root.left && !root.right) return [root.value + parent];
 
   const left = root.left ? branchSums(root.left, root.value + parent) : [];
   const right = root.right ? branchSums(root.right, root.value + parent) : [];
 
   return left.concat(right);
-
 }
 
-module.exports = { BinaryTree, branchSums };
+module.exports = branchSums;
