c8bcd328e4818807c914002a8098360a885c553f
tonyjhuang <huangtonyj91@gmail.com>
2020-01-05 10:08:23 -0800

    landPerimeter.test.js

---
+++ test/problems/landPerimeter.test.js
@@ -0,0 +1,28 @@
+const landPerimeter = require('../../problems/landPerimeter');
+
+describe('landPerimeter()', () => {
+
+  test('Test Case #1', () => {
+    const landMatrix = [
+      [0, 1, 1, 0],
+      [1, 1, 1, 0],
+      [0, 1, 1, 0],
+      [0, 0, 1, 0]
+    ];
+
+    expect(landPerimeter(landMatrix)).toEequal(14);
+  });
+
+  test('Test Case #2', () => {
+    const landMatrix = [
+      [0, 1, 1, 0],
+      [0, 1, 1, 0],
+      [0, 1, 1, 0],
+      [0, 0, 1, 0]
+    ];
+
+    expect(landPerimeter(landMatrix)).toEequal(12);
+  });
+  
+
+});  
