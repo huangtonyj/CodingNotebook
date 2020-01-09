2b076a59fd30446ffd3abf0452bc96a3cd4c1e33
tonyjhuang <huangtonyj91@gmail.com>
2020-01-08 22:26:05 -0800

    refactor minNumberOfCoinsForChange()

--- problems/minNumberOfCoinsForChange.js
+++ problems/minNumberOfCoinsForChange.js
@@ -10,34 +10,23 @@ function minNumberOfCoinsForChange(n, denoms) {
     });
   });
 
-  let current = queue.shift();
-  let breakCounter = 0;
+  let current = true;
 
-  while (current.total !== n && queue.length !== 0 && current.count > 0) {
-    if (current.total > n) breakCounter++;
-    
-    // console.table({total: current.total, count: current.count, breakCounter});
-
-
-    denoms.forEach((denom) => {
-      queue.push({
-        total: denom + current.total,
-        count: 1 + current.count
-      });
-    });
-
+  while (current.total !== n && queue.length) {
     current = queue.shift();
+    if (current.total > n) { continue; }
 
-    if (breakCounter === n ** denoms.length) current.count = -1;
-  }
+    for (let denom of denoms) {
+      const total = current.total + denom;
+      const count = current.count + 1;
 
-  return current.count;
+      queue.push({total, count});
+    }
+  } 
 
+  return queue.length ? current.count : -1;
 }
 
-// console.log(minNumberOfCoinsForChange(11, [1, 5, 10]));
-// console.log(minNumberOfCoinsForChange(7, [2, 4]), -1);
-// console.log(minNumberOfCoinsForChange(4, [1, 5, 10]), 4);
-console.log(minNumberOfCoinsForChange(10, [1, 5, 10]), 1);
-
 module.exports = minNumberOfCoinsForChange;
+
+// Could be further improved by adding a set to track current.total that was seen before.
