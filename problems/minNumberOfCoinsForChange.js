f5598dd5d8eb4fcdf20c9315ec98e759f98b40ed
tonyjhuang <huangtonyj91@gmail.com>
2020-01-08 22:03:10 -0800

    refactor minNumberOfCoinsForChange()

--- problems/minNumberOfCoinsForChange.js
+++ problems/minNumberOfCoinsForChange.js
@@ -1,5 +1,3 @@
-console.log(4  ** 3)
-
 function minNumberOfCoinsForChange(n, denoms) {
   if  (n === 0) return 0;
   
@@ -15,33 +13,25 @@ function minNumberOfCoinsForChange(n, denoms) {
   let current = queue.shift();
   let breakCounter = 0;
 
-  while (current.total !== n && queue.length !== 0 && breakCounter >= 0) {
-    // breakCounter = 0;
-    
+  while (current.total !== n && queue.length !== 0 && current.count > 0) {
     if (current.total > n) breakCounter++;
-    // if (breakCounter > denoms.length * current.count) breakCounter = -1;
-    if (breakCounter === n ** denoms.length) breakCounter = -1;
+    
     // console.table({total: current.total, count: current.count, breakCounter});
 
 
     denoms.forEach((denom) => {
-      
       queue.push({
         total: denom + current.total,
         count: 1 + current.count
       });
-      
-
-
-
     });
 
     current = queue.shift();
+
+    if (breakCounter === n ** denoms.length) current.count = -1;
   }
 
-  // return breakCounter > denoms.length ? current.count : -1;
-  // return current.count;
-  return breakCounter >= 0 ? current.count : -1;
+  return current.count;
 
 }
 
