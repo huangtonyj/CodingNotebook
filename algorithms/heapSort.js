40e016623ec8354218d2ac46d2d2175512d15b8b
tonyjhuang <huangtonyj91@gmail.com>
2020-01-08 22:40:27 -0800

    heapSort.test.js

--- algorithms/heapSort.js
+++ algorithms/heapSort.js
@@ -13,11 +13,4 @@ function heapSort (arr, sort) {
   return result;
 }
 
-// Test
-const arr = [3,5,4,1,2,0];
-console.log(heapSort(arr), [0, 1, 2, 3, 4, 5]);
-console.log(heapSort(arr, (a, b) => b - a), [5, 4, 3, 2, 1, 0]);
-
-const arr2 = [3,5,4,1,2,0,6];
-console.log(heapSort(arr2), [0, 1, 2, 3, 4, 5, 6]);
-console.log(heapSort(arr2, (a, b) => b - a), [6, 5, 4, 3, 2, 1, 0]);
+module.exports = heapSort;
