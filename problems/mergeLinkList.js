c05071551a49de88a798462277f822b7dbcb0811
tonyjhuang <huangtonyj91@gmail.com>
2020-02-06 00:45:08 -0800

    mergeLinkList()

--- problems/mergeLinkList.js
+++ problems/mergeLinkList.js
@@ -1,52 +1,12 @@
-// const nodeLL = require('../dataStructures/nodeLL');
-
-class LinkedList {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-
-  addMany(values) {
-    let current = this;
-    while (current.next !== null) {
-      current = current.next;
-    }
-    for (const value of values) {
-      current.next = new LinkedList(value);
-      current = current.next;
-    }
-    return this;
-  }
-
-  getNodesInArray() {
-    const nodes = [];
-    let current = this;
-    while (current !== null) {
-      nodes.push(current.value);
-      current = current.next;
-    }
-    return nodes;
-  }
-}
-
 function mergeLinkList (head1, head2) {
+  let current = head1.value < head2.value ? head1 : head2;
+  let ans = current;
 
-	let current1 = head1;
-	let current2 = head2;
+  let current1 = current === head1 ? head1.next : head1;
+  let current2 = current === head2 ? head2.next : head2;
 
-	let ans = new LinkedList(null);
-	let current = ans;
-
-	while (current1 || current2) {
-    // console.log(current);
-
-    if (!current1) {
-      current.next = current2;
-      current2 = current2.next;
-    } else if (!current2) {
-      current.next = current1;
-      current1 = current1.next;
-    } else if (current1.value < current2.value) {
+	while (current1 && current2) {
+    if (current1.value < current2.value) {
 	    current.next = current1;
       current1 = current1.next;
 	  } else {
@@ -56,18 +16,10 @@ function mergeLinkList (head1, head2) {
     current = current.next;
 	}
 
-  console.log('ans: ', ans.next);
+  const rest = current1 ? current1 : current2;
+  current.next = rest;
 
-	return ans.next;
-
+	return ans;
 }
-
-// const linkList1 = new nodeLL(2);
-//   linkList1.insert(6).insert(7).insert(8);
-  
-// const linkList2 = new nodeLL(1);
-//   linkList2.insert(3).insert(4).insert(5).insert(9);
-
-// console.log(mergeLinkList(linkList1, linkList2));
 
 module.exports = mergeLinkList;
