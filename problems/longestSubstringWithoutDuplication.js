9bf7d15f693e9165f7017e2740eac9da346b7cfe
tonyjhuang <huangtonyj91@gmail.com>
2020-02-15 23:57:02 -0800

    longestSubstringWithoutDuplication()

--- problems/longestSubstringWithoutDuplication.js
+++ problems/longestSubstringWithoutDuplication.js
@@ -1,36 +1,24 @@
 function longestSubstringWithoutDuplication(str) {
   const hash = {};
-  let pointer = 0;
+  let startPointer = 0;
   let longest = '';
 
   str.split('').forEach((char, idx) => {
-    // console.log(char, idx);
-
     const prevCharIdx = hash[char];
 
-    if (prevCharIdx && prevCharIdx > pointer) {
-      const current = str.slice(pointer, idx);
-      console.log(current);
-
-      if (current.length > longest.length) longest = current;
-      
-      pointer = prevCharIdx + 1;
+    if (prevCharIdx !== undefined && prevCharIdx >= startPointer) {
+      const current = str.slice(startPointer, idx);
+      if (current.length > longest.length) longest = current;  
+      startPointer = prevCharIdx + 1;
     }
 
     hash[char] = idx;
-
   });
 
-  const lastSegment = str.slice(pointer);
-
+  const lastSegment = str.slice(startPointer);
   if (lastSegment.length > longest.length) longest = lastSegment;
 
   return longest;
 }
 
 module.exports = longestSubstringWithoutDuplication;
-
-
-console.log(longestSubstringWithoutDuplication('abcdeabcdefc'));
-// console.log(longestSubstringWithoutDuplication('a'));
-// console.log(longestSubstringWithoutDuplication('clementisacap'));
