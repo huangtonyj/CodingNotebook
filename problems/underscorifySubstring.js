073ae3af9cf37be156dafe191af27218e594a539
tonyjhuang <huangtonyj91@gmail.com>
2020-02-17 14:50:40 -0800

    underscorifySubstr()

--- problems/underscorifySubstring.js
+++ problems/underscorifySubstring.js
@@ -1,50 +1,68 @@
 /*  
-  Write a function that takes in two strings: a main string and a potential substring of the main string. 
-  The function should return a version of the main string with every instance of the substring in it wrapped between underscores. 
-  If two instances of the substring in the main string overlap each other or sit side by side, 
-  the underscores relevant to these two substrings should only appear on the far left of the left substring and on the far right of the right substring. 
-  If the main string does not contain the other string at all, return the main string intact.
+  Write a function that takes in two strs: a main str and a potential substr of the main str. 
+  The function should return a version of the main str with every instance of the substr in it wrapped between underscores. 
+  If two instances of the substr in the main str overlap each other or sit side by side, 
+  the underscores relevant to these two substrs should only appear on the far left of the left substr and on the far right of the right substr. 
+  If the main str does not contain the other str at all, return the main str intact.
 */
 
-const inputStr = "testthis is a testtest to see if testestest it works";
-const inputSubStr = "test";
-const output = "_test_this is a _testtest_ to see if _testestest_ it works";
+// const inputStr = "testthis is a testtest to see if testestest it works";
+// const inputSubStr = "test";
+// const output = "_test_this is a _testtest_ to see if _testestest_ it works";
 
-function underscorifySubstring(string, substring) {
+function underscorifySubstr(str, substr) {
 
-  let result = '';
+  const locations = [];
   let idx = 0;
-  const span = substring.length;
-  let prevMatch = false;
+  const span = substr.length;
 
-  while (idx <= string.length) {
-    const current = string.slice(idx, idx + span);
-    const char = string[idx];
 
-    console.table({char, current, prevMatch, result});
+  // get indicies where "substr exist"
+  while (idx < str.length) {
+    const current = str.slice(idx, idx + span);
 
-    if (current === substring) {
-      if (!prevMatch) result += '_';
-
-      prevMatch = true;
-      result += substring.slice(0, span - 1);
-      idx += span - 1;
-    } else if (prevMatch) {
-      prevMatch = false;
-      result += char;
-      result += '_';
-      idx++;
-    } else {
-      result += char;
-      idx++;
+    if (current === substr) {
+      locations.push(idx);
     }
-    
+
+    idx++;
   }
-  
-  return result;
-  
+
+  // group locations if they are close to each other;
+  const groupedLocations = [];
+  let startIdx = null;
+  for (let i = 0; i < locations.length; i++) {
+    if (startIdx === null) {
+      startIdx = locations[i];
+    }
+    // console.log(startIdx)
+
+    const currentIdx = locations[i];
+    const nextIdx = locations[i + 1];
+
+    
+    if ((nextIdx - currentIdx > span) || !nextIdx) {
+      groupedLocations.push([startIdx, currentIdx + span]);
+      startIdx = null;
+    }
+  }
+
+  let result = str.slice(0, locations[0]);
+
+  // Build the resulting string;
+  groupedLocations.forEach((pair, jdx, arr) => {
+    const [start, end] = pair;
+    result += `_${str.slice(start,end)}_`;
+
+    if (arr[jdx + 1]) {
+      const nextStart = arr[jdx + 1][0];
+      result += str.slice(end,nextStart);
+    } else {
+      result += str.slice(end);
+    }
+  });
+
+  return result;  
 }
 
-module.exports = underscorifySubstring;
-
-console.log(underscorifySubstring(inputStr, inputSubStr));
+module.exports = underscorifySubstr;
