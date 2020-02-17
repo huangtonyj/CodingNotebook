7db04c2fdb6f5e92a579e6fa7314b7ab55fbdc0d
tonyjhuang <huangtonyj91@gmail.com>
2020-02-17 15:29:10 -0800

    refactor

--- problems/underscorifySubstring.js
+++ problems/underscorifySubstring.js
@@ -6,63 +6,62 @@
   If the main str does not contain the other str at all, return the main str intact.
 */
 
-// const inputStr = "testthis is a testtest to see if testestest it works";
-// const inputSubStr = "test";
-// const output = "_test_this is a _testtest_ to see if _testestest_ it works";
+const inputStr = "testthis is a testtest to see if testestest it works";
+const inputSubStr = "test";
+const output = "_test_this is a _testtest_ to see if _testestest_ it works";
 
 function underscorifySubstr(str, substr) {
 
-  const locations = [];
+  const occurences = getOccurences(str, substr);
+  const groupedOccurences = getGroupedOccurences(occurences, substr.length);
+
+  return groupedOccurences.reduce((result, occurence) => {
+    const [start, end, next] = occurence;
+    return result + `_${str.slice(start,end)}_${str.slice(end, next)}`;
+  }, str.slice(0, occurences[0]));
+}
+
+const getOccurences = (str, substr) => {
+  const occurences = [];
   let idx = 0;
   const span = substr.length;
-
 
   // get indicies where "substr exist"
   while (idx < str.length) {
     const current = str.slice(idx, idx + span);
 
     if (current === substr) {
-      locations.push(idx);
+      occurences.push(idx);
     }
 
     idx++;
   }
 
-  // group locations if they are close to each other;
-  const groupedLocations = [];
-  let startIdx = null;
-  for (let i = 0; i < locations.length; i++) {
-    if (startIdx === null) {
-      startIdx = locations[i];
-    }
-    // console.log(startIdx)
-
-    const currentIdx = locations[i];
-    const nextIdx = locations[i + 1];
-
-    
-    if ((nextIdx - currentIdx > span) || !nextIdx) {
-      groupedLocations.push([startIdx, currentIdx + span]);
-      startIdx = null;
-    }
-  }
-
-  let result = str.slice(0, locations[0]);
-
-  // Build the resulting string;
-  groupedLocations.forEach((pair, jdx, arr) => {
-    const [start, end] = pair;
-    result += `_${str.slice(start,end)}_`;
-
-    if (arr[jdx + 1]) {
-      const nextStart = arr[jdx + 1][0];
-      result += str.slice(end,nextStart);
-    } else {
-      result += str.slice(end);
-    }
-  });
-
-  return result;  
+  return occurences;
 }
 
+const getGroupedOccurences = (occurences, size) => {
+    // group Occurences if they are close to each other;
+    const groupedOccurences = [];
+    let startIdx = null;
+    for (let i = 0; i < occurences.length; i++) {
+      if (startIdx === null) {
+        startIdx = occurences[i];
+      }
+
+      const currentIdx = occurences[i];
+      const nextIdx = occurences[i + 1];
+
+
+      if ((nextIdx - currentIdx > size) || !nextIdx) {
+        groupedOccurences.push([startIdx, currentIdx + size, nextIdx]);
+        startIdx = null;
+      }
+    }
+
+    return groupedOccurences;
+};
+
 module.exports = underscorifySubstr;
+
+console.log(underscorifySubstr(inputStr, inputSubStr));
