9b32bdcd2a6e0d68ec320055654ec80f078b2949
tonyjhuang <huangtonyj91@gmail.com>
2020-02-21 14:07:14 -0800

    clean up patternMatcher()

--- problems/patternMatcher.js
+++ problems/patternMatcher.js
@@ -1,65 +1,60 @@
 function patternMatcher(pattern, string) {
-  const counts = {
-    x: 0,
-    y: 0
-  };
+  const counts = getCounts(pattern);
 
-  pattern.split('').forEach((char) => {
-    counts[char]++;
-  });
-
+  // Get possible combination of lens of y given looping x and string.length;
+  // Check if possible pattern substring of X and Y matches up
   for (let xLength = 0; xLength <= string.length / counts['x']; xLength++) {
     const yLength = (string.length - counts['x'] * xLength) / counts['y'] || 0;
 
     if (!Number.isInteger(yLength)) { continue; }
 
-    let idx = 0;
-    let log = {};
+    let strIdx = 0;
+    let patternDict = {};
     let currentXCount = 0;
     let currentYCount = 0;
 
     for (const char of pattern.split('')) {
-      const length = char === 'x' ? xLength : yLength;
-
       if (char ==='x') {
         currentXCount++;
       } else {
         currentYCount++;
       }
       
-      const current = string.slice(idx, idx + length);
+      const length = char === 'x' ? xLength : yLength;
+      const current = string.slice(strIdx, strIdx + length);
 
+      strIdx += length;
       if (current === '' && counts[char] !== 0) break;
 
-
-      idx += length;
-
-      if (!log[char]) {
-        log[char] = current;
-      } else if (log[char] !== current) {
-        log = null;
+      if (!patternDict[char]) {
+        patternDict[char] = current;
+      } else if (patternDict[char] !== current) {
+        patternDict = null;
         break;
       }
-
     }
 
-    if (log && currentXCount === counts['x'] && currentYCount === counts['y']) {
-      log.x = log.x || '';
-      log.y = log.y || '';
-      return log;
+    if (patternDict && currentXCount === counts['x'] && currentYCount === counts['y']) {
+      patternDict.x = patternDict.x || '';
+      patternDict.y = patternDict.y || '';
+      return patternDict;
     }
   }
 
   return {};
 }
 
-module.exports = patternMatcher;
+const getCounts = (str) => {
+  const counts = {
+    x: 0,
+    y: 0
+  };
 
-// console.log(patternMatcher('yxx', 'yomama'), ['ma', 'yo']);
-// console.log(patternMatcher('yyxyyx', 'gogopowerrangergogopowerranger'), ['ma', 'yo']);
-// console.log(patternMatcher(
-//   'xyxxxyyx',
-//   'baddaddoombaddaddoomibaddaddoombaddaddoombaddaddoombaddaddoomibaddaddoomibaddaddoom'
-// ), ['baddaddoom', 'baddaddoomi']);
-// console.log(patternMatcher('xxxx', 'testtesttesttest'), ['test', '']);  
-// console.log(patternMatcher('yyyy', 'testtesttesttest'), ['', 'test']);
+  str.split('').forEach((char) => {
+    counts[char]++;
+  });
+
+  return counts;
+};
+
+module.exports = patternMatcher;
