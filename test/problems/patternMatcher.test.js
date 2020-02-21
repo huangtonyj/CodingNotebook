dbd95c1bd23867c88fe7c92115d8be6a5ed4fc01
tonyjhuang <huangtonyj91@gmail.com>
2020-02-20 22:59:59 -0800

    Update to return object instead of array

--- test/problems/patternMatcher.test.js
+++ test/problems/patternMatcher.test.js
@@ -2,59 +2,59 @@ const patternMatcher = require('../../problems/patternMatcher');
 
 describe('patternMatcher()', () => {
   test('Test Case #1', () => {
-    expect(patternMatcher('xyxy', 'abab')).toEqual(['a', 'b']);
+    expect(patternMatcher('xyxy', 'abab')).toEqual({x: 'a', y: 'b'});
   });
 
   test('Test Case #2', () => {
-    expect(patternMatcher('yxyx', 'abab')).toEqual(['b', 'a']);
+    expect(patternMatcher('yxyx', 'abab')).toEqual({x: 'b', y: 'a'});
   });
 
   test('Test Case #3', () => {
-    expect(patternMatcher('yxx', 'yomama')).toEqual(['ma', 'yo']);
+    expect(patternMatcher('yxx', 'yomama')).toEqual({x: 'ma', y: 'yo'});
   });
 
   test('Test Case #4', () => {
-    expect(patternMatcher('xxyxxy', 'gogopowerrangergogopowerranger')).toEqual(['go', 'powerranger']);
+    expect(patternMatcher('xxyxxy', 'gogopowerrangergogopowerranger')).toEqual({x: 'go', y: 'powerranger'});
   });
 
   test('Test Case #5', () => {
-    expect(patternMatcher('yyxyyx', 'gogopowerrangergogopowerranger')).toEqual(['powerranger', 'go']);
+    expect(patternMatcher('yyxyyx', 'gogopowerrangergogopowerranger')).toEqual({x: 'powerranger', y: 'go'});
   });
 
   test('Test Case #6', () => {
     expect(patternMatcher(
       'xyxxxyyx',
       'baddaddoombaddaddoomibaddaddoombaddaddoombaddaddoombaddaddoomibaddaddoomibaddaddoom'
-    )).toEqual(['baddaddoom', 'baddaddoomi']);
+    )).toEqual({x: 'baddaddoom', y: 'baddaddoomi'});
   });
 
   test('Test Case #7', () => {
     expect(patternMatcher(
       'yxyyyxxy',
       'baddaddoombaddaddoomibaddaddoombaddaddoombaddaddoombaddaddoomibaddaddoomibaddaddoom'
-    )).toEqual(['baddaddoomi', 'baddaddoom']);
+    )).toEqual({x: 'baddaddoomi', y: 'baddaddoom'});
   });
 
   test('Test Case #8', () => {
-    expect(patternMatcher('xxyxyy', 'testtestwrongtestwrongtest')).toEqual([]);
+    expect(patternMatcher('xxyxyy', 'testtestwrongtestwrongtest')).toEqual({});
   });
 
   test('Test Case #9', () => {
     expect(patternMatcher(
       'xyxxxyyx',
       'baddaddoombaddadoomibaddaddoombaddaddoombaddaddoombaddaddoomibaddaddoomibaddaddoom'
-    )).toEqual([]);
+    )).toEqual({});
   });
 
   test('Test Case #10', () => {
-    expect(patternMatcher('xyx', 'thisshouldobviouslybewrong')).toEqual([]);
+    expect(patternMatcher('xyx', 'thisshouldobviouslybewrong')).toEqual({});
   });
 
   test('Test Case #11', () => {
-    expect(patternMatcher('xxxx', 'testtesttesttest')).toEqual(['test', '']);
+    expect(patternMatcher('xxxx', 'testtesttesttest')).toEqual({x: 'test', y: ''});
   });
 
   test('Test Case #12', () => {
-    expect(patternMatcher('yyyy', 'testtesttesttest')).toEqual(['', 'test']);
+    expect(patternMatcher('yyyy', 'testtesttesttest')).toEqual({x: '', y: 'test'});
   });
 });
