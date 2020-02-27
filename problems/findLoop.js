c12a2a500dd6761982ec0d9ccf05602e993d4f89
tonyjhuang <huangtonyj91@gmail.com>
2020-02-27 09:35:01 -0800

    Optimize findLoop()

--- problems/findLoop.js
+++ problems/findLoop.js
@@ -1,6 +1,27 @@
+// O(n) time O(1) space
 function findLoop(head) {
-  const hash = {};
+  let turtle = head.next;
+  let rabbit = head.next.next;
 
+  while (turtle !== rabbit) {
+    turtle = turtle.next;
+    rabbit  = rabbit.next.next;
+  }
+
+  turtle = head;
+
+  while (turtle !== rabbit) {
+    turtle = turtle.next;
+    rabbit = rabbit.next;
+  }
+
+  return turtle;
+}
+
+
+// O(n) time O(n) space
+function findLoop2(head) {
+  const hash = {};
   let current = head;
 
   while (current) {
