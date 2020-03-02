986a0b042f6ed3239eb68210cd26d55aed47598d
tonyjhuang <huangtonyj91@gmail.com>
2020-03-01 23:55:10 -0800

    Add question

--- problems/maxPathSum.js
+++ problems/maxPathSum.js
@@ -1,3 +1,17 @@
+/* 
+  Write a function that takes in a Binary Tree and returns its max path sum.
+  A path is a collection of connected nodes where no node is connected to more than two other nodes;
+  a path sum is the sum of the values of the nodes in a particular path.
+
+      1
+     /\
+    2  3
+   /\  /\
+  4 5 6 7
+
+  4 + 2 + 1 + 4 + 7 = 18
+*/
+
 function maxPathSum(tree, hasParent = false) {
   if (!tree) return [0, 0];
 
