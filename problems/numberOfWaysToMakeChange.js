9357685b7abba13f9d5b219de8f4a2c33db39178
tonyjhuang <huangtonyj91@gmail.com>
2020-03-02 21:44:12 -0800

    numberOfWaysToMakeChange()

---
+++ problems/numberOfWaysToMakeChange.js
@@ -0,0 +1,15 @@
+function numberOfWaysToMakeChange(n, denoms = [1, 5, 10, 25]) {
+  const ways = new Array(n + 1).fill(0);
+
+  ways[0] = 1;
+
+  for (let denom of denoms) {
+    for (let amount = 1; amount < n + 1; amount++) {
+      if (denom <= amount) ways[amount] += ways[amount - denom];
+    }
+  }
+
+  return ways[n];
+}
+
+module.exports = numberOfWaysToMakeChange;
