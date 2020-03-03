3d68b30674b28c40263505d84ed095a10032f5f2
tonyjhuang <huangtonyj91@gmail.com>
2020-03-02 21:42:15 -0800

    refactor()

--- problems/maxSubsetSumNoAdjacent.js
+++ problems/maxSubsetSumNoAdjacent.js
@@ -2,13 +2,15 @@ function maxSubsetSumNoAdjacent(arr) {
   if (arr.length === 0) return 0;
   if (arr.length === 1) return arr[0];
 
-  arr[1] = Math.max(arr[0], arr[1]);
+  const result = arr.map(el => el);
+
+  result[1] = Math.max(result[0], result[1]);
   
-  for (let i = 2; i < arr.length; i++) {
-    arr[i] = Math.max(arr[i-1], arr[i] + arr[i - 2]);
+  for (let i = 2; i < result.length; i++) {
+    result[i] = Math.max(result[i-1], result[i] + result[i - 2]);
   }
 
-  return arr[arr.length - 1];
+  return result[arr.length - 1];
 }
 
 module.exports = maxSubsetSumNoAdjacent;
