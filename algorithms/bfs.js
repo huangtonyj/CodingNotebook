83d6757e2159d2a2b6589f0b46c1fd697519b6bd
tonyjhuang <huangtonyj91@gmail.com>
2020-10-25 21:56:20 -0700

    refactor bfs

--- algorithms/bfs.js
+++ algorithms/bfs.js
@@ -4,10 +4,9 @@ const NodeBT = require('../dataStructures/NodeBT');
 function bfs(node) {
   const visitedQueue = new Queue([node]);
   const result = [];
-  let currentNode;
 
   while (visitedQueue.length > 0) {
-    currentNode = visitedQueue.dequeue();
+    const currentNode = visitedQueue.dequeue();
     result.push(currentNode.value);
 
     if (currentNode.left) { visitedQueue.enqueue(currentNode.left); }
@@ -19,19 +18,19 @@ function bfs(node) {
 
 module.exports = bfs;
 
-// const rootNode = new NodeBT(4);
-//   rootNode.left = new NodeBT(2);
-//     rootNode.left.left = new NodeBT(1);
-//     rootNode.left.right = new NodeBT(3);
-//   rootNode.right = new NodeBT(6);
-//     rootNode.right.left = new NodeBT(5);
-//     rootNode.right.right = new NodeBT(7);
+const rootNode = new NodeBT(4);
+  rootNode.left = new NodeBT(2);
+    rootNode.left.left = new NodeBT(1);
+    rootNode.left.right = new NodeBT(3);
+  rootNode.right = new NodeBT(6);
+    rootNode.right.left = new NodeBT(5);
+    rootNode.right.right = new NodeBT(7);
 //      4
 //    /   \
 //   2     6
 //  / \   / \
 // 1   3 5   7
 
-// console.log(bfs(rootNode), [ 4, 2, 6, 1, 3, 5, 7]);
+console.log(bfs(rootNode), [ 4, 2, 6, 1, 3, 5, 7]);
 
 
