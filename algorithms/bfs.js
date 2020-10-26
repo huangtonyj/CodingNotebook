389cde2d3dcd465509bdf182610e0da147462629
tonyjhuang <huangtonyj91@gmail.com>
2020-10-25 21:56:41 -0700

    refactor bfs

--- algorithms/bfs.js
+++ algorithms/bfs.js
@@ -18,19 +18,19 @@ function bfs(node) {
 
 module.exports = bfs;
 
-const rootNode = new NodeBT(4);
-  rootNode.left = new NodeBT(2);
-    rootNode.left.left = new NodeBT(1);
-    rootNode.left.right = new NodeBT(3);
-  rootNode.right = new NodeBT(6);
-    rootNode.right.left = new NodeBT(5);
-    rootNode.right.right = new NodeBT(7);
+// const rootNode = new NodeBT(4);
+//   rootNode.left = new NodeBT(2);
+//     rootNode.left.left = new NodeBT(1);
+//     rootNode.left.right = new NodeBT(3);
+//   rootNode.right = new NodeBT(6);
+//     rootNode.right.left = new NodeBT(5);
+//     rootNode.right.right = new NodeBT(7);
 //      4
 //    /   \
 //   2     6
 //  / \   / \
 // 1   3 5   7
 
-console.log(bfs(rootNode), [ 4, 2, 6, 1, 3, 5, 7]);
+// console.log(bfs(rootNode), [ 4, 2, 6, 1, 3, 5, 7]);
 
 
