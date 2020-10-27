714d353e66d6d60aab404558b7e0deb4f7ed201f
tonyjhuang <huangtonyj91@gmail.com>
2020-10-26 20:07:05 -0700

    refactor

--- algorithms/insertionSort.js
+++ algorithms/insertionSort.js
@@ -1,9 +1,7 @@
 function insertionSort(arr, sortFn = (a, b) => a - b) {
-  let key, j;
-
   for (let i = 1; i < arr.length; i++) {
-    key = arr[i];
-    j = i - 1;
+    const key = arr[i];
+    let j = i - 1;
     while (j >= 0 && sortFn(key, arr[j]) < 0) {
       arr[j+1] = arr[j];
       j -= 1;
