3f0b5b636406905e9e90fd01169a5323b84cda5d
tonyjhuang <huangtonyj91@gmail.com>
2020-11-29 12:06:37 -0800

    minNumberOfCoinsForChange2()

--- problems/minNumberOfCoinsForChange.js
+++ problems/minNumberOfCoinsForChange.js
@@ -19,13 +19,34 @@ function minNumberOfCoinsForChange(n, denoms) {
       const count = current.count + 1;
 
       queue.push({total, count});
+      console.log(current, queue);
     }
   } 
 
   return queue.length ? current.count : -1;
 }
 
+function minNumberOfCoinsForChange2(n, denoms) {
+  const numOfCoins = new Array(n + 1).fill(Infinity);
+  numOfCoins[0] = 0;
+
+  for (const denom of denoms) {
+    for (let amount = 0; amount < numOfCoins.length; amount++) {
+      if (denom <= amount) {
+        numOfCoins[amount] = Math.min(numOfCoins[amount], numOfCoins[amount - denom] + 1);
+        // console.log(denom, numOfCoins);
+      }
+    }
+  }
+
+  return numOfCoins[n] !== Infinity ? numOfCoins[n] : -1;
+}
+
+
 module.exports = minNumberOfCoinsForChange;
 
 // Simply a BFS search
 // Improvement: Add a set to track current.total that was seen before.
+
+// minNumberOfCoinsForChange(25, [1, 5, 10]);
+// minNumberOfCoinsForChange2(25, [1, 5, 10]);
