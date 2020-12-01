825e1d801983b0125c76413151a3463cb7d58632
tonyjhuang <huangtonyj91@gmail.com>
2020-12-01 00:11:56 -0800

    indexEqualsValue() brute O(n)

---
+++ problems/indexEqualsValue.js
@@ -0,0 +1,27 @@
+/*
+  Write a function that takes in a sorted array of distinct integers and returns
+  the first index in the array that is equal to the value at that index. In
+  other words, your function should return the minimum index where
+  index == array[index].
+  If there is no such index, your function should return -1 .
+
+  Sample Input
+  array = [-5, -3, 0, 3, 4, 5, 9]
+
+  Sample Output
+  3 === array[3]
+
+*/
+
+
+
+// O(n) time O(1) space
+function indexEqualsValue(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const el = arr[i];
+    if (el === i) return i;
+  }
+
+  return -1;
+}
+
