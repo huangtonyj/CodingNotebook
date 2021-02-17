4d727490e5e4bfba5a08090ff3c62f6a586bf2f7
tonyjhuang <huangtonyj91@gmail.com>
2021-02-17 10:06:07 -0800

    Add prompt

--- problems/findLoop.js
+++ problems/findLoop.js
@@ -1,3 +1,24 @@
+/*
+  Write a function that takes in the head of a Singly Linked List that contains
+  a loop (in other words, the list's tail node points to some node in the list
+  instead of None / null). The function should return
+  the node (the actual node--not just its value) from which the loop originates
+  in constant space.
+
+  Each LinkedList node has an integer value as well as
+  a next node pointing to the next node in the list.
+
+  Sample Input
+  head = 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 // the head node with value 0
+                            ^         v
+                            9 <- 8 <- 7
+
+  Sample Output
+  4 -> 5 -> 6 // the node with value 4
+  ^         v
+  9 <- 8 <- 7
+*/
+
 // O(n) time O(1) space
 function findLoop(head) {
   let turtle = head.next;
