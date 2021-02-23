1c96920256ef343e2220958e862ed98ed30b2fcd
tonyjhuang <huangtonyj91@gmail.com>
2021-02-22 17:33:26 -0800

    star problem

--- problems/patternMatcher.js
+++ problems/patternMatcher.js
@@ -1,3 +1,36 @@
+// ***
+/*
+  You're given two non-empty strings. The first one is a pattern consisting of
+  only "x"s and / or "y"s; the other one is a normal
+  string of alphanumeric characters. Write a function that checks whether the
+  normal string matches the pattern.
+
+  A string S0 is said to match a pattern if replacing all
+  "x"s in the pattern with some non-empty substring
+  S1 of S0 and replacing all "y"s in the
+  pattern with some non-empty substring S2 of
+  S0 yields the same string S0.
+
+  If the input string doesn't match the input pattern, the function should
+  return an empty array; otherwise, it should return an array holding the
+  strings S1 and S2 that represent
+  "x" and "y" in the normal string, in that order. If
+  the pattern doesn't contain any "x"s or "y"s, the
+  respective letter should be represented by an empty string in the final array
+  that you return.
+
+  You can assume that there will never be more than one pair of strings
+  S1 and S2 that appropriately represent
+  "x" and "y" in the normal string.
+
+  Sample Input
+    pattern = "xxyxxy"
+    string = "gogopowerrangergogopowerranger"
+
+  Sample Output
+    ["go", "powerranger"]
+*/
+
 function patternMatcher(pattern, string) {
   const counts = getCounts(pattern);
 
