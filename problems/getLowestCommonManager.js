47746ca31c0c58f00ebb11eeb65b79656ce00b95
tonyjhuang <huangtonyj91@gmail.com>
2021-02-25 10:00:03 -0800

    star problem, add commentary, refactor

--- problems/getLowestCommonManager.js
+++ problems/getLowestCommonManager.js
@@ -1,22 +1,22 @@
+// ***
 function getLowestCommonManager(manager, report1, report2) {
-
   let result = {};
-  
+
   if (manager === report1) result.report1= manager
   if (manager === report2) result.report2= manager
 
-  manager.directReports.forEach((directReport) => {
+  for (const directReport of manager.directReports) {
     result = {...result, ...getLowestCommonManager(directReport, report1, report2)};
-  });
+  }
 
   if (result.manager) return result.manager;
   
   if (result.report1 && result.report2) {
-    result.manager = manager;
+    result.manager = manager; // found LCM
   } else if (result.report1) {
-    result.report1 = manager;
+    result.report1 = manager; // recursively report LCM to root
   } else if (result.report2) { 
-    result.report2 = manager;
+    result.report2 = manager; // recursively report LCM to root
   }
 
   return result.manager ? result.manager : result;
