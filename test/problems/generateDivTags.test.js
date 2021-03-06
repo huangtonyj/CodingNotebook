abd3a5853c350d7aeb2854d15d9646fd1db2ac63
tonyjhuang <huangtonyj91@gmail.com>
2021-03-05 20:47:09 -0800

    generateDivTags.test

--- test/problems/generateDivTags.test.js
+++ test/problems/generateDivTags.test.js
@@ -1,94 +1,96 @@
 const generateDivTags = require('../../problems/generateDivTags');
 
+describe('generateDivTags()', () => {
 
-it('Test Case 1', () => {
-  const expected = [
-    '<div><div><div></div></div></div>',
-    '<div><div></div><div></div></div>',
-    '<div><div></div></div><div></div>',
-    '<div></div><div><div></div></div>',
-    '<div></div><div></div><div></div>'
-  ];
-  expect(generateDivTags(3)).toEqual(expected);
-});
-
-it('Test Case 2', () => {
-  const expected = [
-    '<div><div></div></div>', 
-    '<div></div><div></div>' 
-  ];
-  expect(generateDivTags(2)).toEqual(expected);
-});
-
-it('Test Case 3', () => {
-  const expected = ['<div></div>'];
-  expect(generateDivTags(1)).toEqual(expected);
-});
-
-it('Test Case 4', () => {
-  const expected = [
-    '<div><div><div><div></div></div></div></div>',
-    '<div><div><div></div><div></div></div></div>',
-    '<div><div><div></div></div><div></div></div>',
-    '<div><div><div></div></div></div><div></div>',
-    '<div><div></div><div><div></div></div></div>',
-    '<div><div></div><div></div><div></div></div>',
-    '<div><div></div><div></div></div><div></div>',
-    '<div><div></div></div><div><div></div></div>',
-    '<div><div></div></div><div></div><div></div>',
-    '<div></div><div><div><div></div></div></div>',
-    '<div></div><div><div></div><div></div></div>',
-    '<div></div><div><div></div></div><div></div>',
-    '<div></div><div></div><div><div></div></div>',
-    '<div></div><div></div><div></div><div></div>'
-  ];
-  expect(generateDivTags(4)).toEqual(expected);
-});
-
-it('Test Case 5', () => {
-  const expected = [
-    '<div><div><div><div><div></div></div></div></div></div>',
-    '<div><div><div><div></div><div></div></div></div></div>',
-    '<div><div><div><div></div></div><div></div></div></div>',
-    '<div><div><div><div></div></div></div><div></div></div>',
-    '<div><div><div><div></div></div></div></div><div></div>',
-    '<div><div><div></div><div><div></div></div></div></div>',
-    '<div><div><div></div><div></div><div></div></div></div>',
-    '<div><div><div></div><div></div></div><div></div></div>',
-    '<div><div><div></div><div></div></div></div><div></div>',
-    '<div><div><div></div></div><div><div></div></div></div>',
-    '<div><div><div></div></div><div></div><div></div></div>',
-    '<div><div><div></div></div><div></div></div><div></div>',
-    '<div><div><div></div></div></div><div><div></div></div>',
-    '<div><div><div></div></div></div><div></div><div></div>',
-    '<div><div></div><div><div><div></div></div></div></div>',
-    '<div><div></div><div><div></div><div></div></div></div>',
-    '<div><div></div><div><div></div></div><div></div></div>',
-    '<div><div></div><div><div></div></div></div><div></div>',
-    '<div><div></div><div></div><div><div></div></div></div>',
-    '<div><div></div><div></div><div></div><div></div></div>',
-    '<div><div></div><div></div><div></div></div><div></div>',
-    '<div><div></div><div></div></div><div><div></div></div>',
-    '<div><div></div><div></div></div><div></div><div></div>',
-    '<div><div></div></div><div><div><div></div></div></div>',
-    '<div><div></div></div><div><div></div><div></div></div>',
-    '<div><div></div></div><div><div></div></div><div></div>',
-    '<div><div></div></div><div></div><div><div></div></div>',
-    '<div><div></div></div><div></div><div></div><div></div>',
-    '<div></div><div><div><div><div></div></div></div></div>',
-    '<div></div><div><div><div></div><div></div></div></div>',
-    '<div></div><div><div><div></div></div><div></div></div>',
-    '<div></div><div><div><div></div></div></div><div></div>',
-    '<div></div><div><div></div><div><div></div></div></div>',
-    '<div></div><div><div></div><div></div><div></div></div>',
-    '<div></div><div><div></div><div></div></div><div></div>',
-    '<div></div><div><div></div></div><div><div></div></div>',
-    '<div></div><div><div></div></div><div></div><div></div>',
-    '<div></div><div></div><div><div><div></div></div></div>',
-    '<div></div><div></div><div><div></div><div></div></div>',
-    '<div></div><div></div><div><div></div></div><div></div>',
-    '<div></div><div></div><div></div><div><div></div></div>',
-    '<div></div><div></div><div></div><div></div><div></div>'
-  ];
-  expect(generateDivTags(5)).toEqual(expected);
+  it('Test Case 1', () => {
+    const expected = [
+      '<div><div><div></div></div></div>',
+      '<div><div></div><div></div></div>',
+      '<div><div></div></div><div></div>',
+      '<div></div><div><div></div></div>',
+      '<div></div><div></div><div></div>'
+    ];
+    expect(generateDivTags(3)).toEqual(expected);
+  });
+  
+  it('Test Case 2', () => {
+    const expected = [
+      '<div><div></div></div>', 
+      '<div></div><div></div>' 
+    ];
+    expect(generateDivTags(2)).toEqual(expected);
+  });
+  
+  it('Test Case 3', () => {
+    const expected = ['<div></div>'];
+    expect(generateDivTags(1)).toEqual(expected);
+  });
+  
+  it('Test Case 4', () => {
+    const expected = [
+      '<div><div><div><div></div></div></div></div>',
+      '<div><div><div></div><div></div></div></div>',
+      '<div><div><div></div></div><div></div></div>',
+      '<div><div><div></div></div></div><div></div>',
+      '<div><div></div><div><div></div></div></div>',
+      '<div><div></div><div></div><div></div></div>',
+      '<div><div></div><div></div></div><div></div>',
+      '<div><div></div></div><div><div></div></div>',
+      '<div><div></div></div><div></div><div></div>',
+      '<div></div><div><div><div></div></div></div>',
+      '<div></div><div><div></div><div></div></div>',
+      '<div></div><div><div></div></div><div></div>',
+      '<div></div><div></div><div><div></div></div>',
+      '<div></div><div></div><div></div><div></div>'
+    ];
+    expect(generateDivTags(4)).toEqual(expected);
+  });
+  
+  it('Test Case 5', () => {
+    const expected = [
+      '<div><div><div><div><div></div></div></div></div></div>',
+      '<div><div><div><div></div><div></div></div></div></div>',
+      '<div><div><div><div></div></div><div></div></div></div>',
+      '<div><div><div><div></div></div></div><div></div></div>',
+      '<div><div><div><div></div></div></div></div><div></div>',
+      '<div><div><div></div><div><div></div></div></div></div>',
+      '<div><div><div></div><div></div><div></div></div></div>',
+      '<div><div><div></div><div></div></div><div></div></div>',
+      '<div><div><div></div><div></div></div></div><div></div>',
+      '<div><div><div></div></div><div><div></div></div></div>',
+      '<div><div><div></div></div><div></div><div></div></div>',
+      '<div><div><div></div></div><div></div></div><div></div>',
+      '<div><div><div></div></div></div><div><div></div></div>',
+      '<div><div><div></div></div></div><div></div><div></div>',
+      '<div><div></div><div><div><div></div></div></div></div>',
+      '<div><div></div><div><div></div><div></div></div></div>',
+      '<div><div></div><div><div></div></div><div></div></div>',
+      '<div><div></div><div><div></div></div></div><div></div>',
+      '<div><div></div><div></div><div><div></div></div></div>',
+      '<div><div></div><div></div><div></div><div></div></div>',
+      '<div><div></div><div></div><div></div></div><div></div>',
+      '<div><div></div><div></div></div><div><div></div></div>',
+      '<div><div></div><div></div></div><div></div><div></div>',
+      '<div><div></div></div><div><div><div></div></div></div>',
+      '<div><div></div></div><div><div></div><div></div></div>',
+      '<div><div></div></div><div><div></div></div><div></div>',
+      '<div><div></div></div><div></div><div><div></div></div>',
+      '<div><div></div></div><div></div><div></div><div></div>',
+      '<div></div><div><div><div><div></div></div></div></div>',
+      '<div></div><div><div><div></div><div></div></div></div>',
+      '<div></div><div><div><div></div></div><div></div></div>',
+      '<div></div><div><div><div></div></div></div><div></div>',
+      '<div></div><div><div></div><div><div></div></div></div>',
+      '<div></div><div><div></div><div></div><div></div></div>',
+      '<div></div><div><div></div><div></div></div><div></div>',
+      '<div></div><div><div></div></div><div><div></div></div>',
+      '<div></div><div><div></div></div><div></div><div></div>',
+      '<div></div><div></div><div><div><div></div></div></div>',
+      '<div></div><div></div><div><div></div><div></div></div>',
+      '<div></div><div></div><div><div></div></div><div></div>',
+      '<div></div><div></div><div></div><div><div></div></div>',
+      '<div></div><div></div><div></div><div></div><div></div>'
+    ];
+    expect(generateDivTags(5)).toEqual(expected);
+  });
 });
