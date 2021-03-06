1ca2f21bd640568111db1eaa6aaae14f0fd96245
tonyjhuang <huangtonyj91@gmail.com>
2021-03-06 14:37:07 -0800

    linkedListUtils()

---
+++ algorithms/linkedListUtils.js
@@ -0,0 +1,51 @@
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+
+    return this;
+  }
+
+  insert(value) {
+    this.next = new LinkedList(value);
+    
+    return this.next;
+  }
+}
+
+function arrayToLinkedList(arr) {
+  const head = new LinkedList(arr[0]);
+  let current = head;
+
+  for (let i = 1; i < arr.length; i++) {
+    const value = arr[i];
+    current = current.insert(value);
+  }
+
+  return head;
+}
+
+function linkedListToArr(head) {
+  const res = [];
+  let current = head;
+
+  while (current) {
+    res.push(current);
+    current = current.next;
+  }
+}
+
+function printLinkedList(head) {
+  let current = head;
+
+  while (current) {
+    console.log(current.value);
+    current = current.next;
+  }
+}
+
+module.exports = {
+  arrayToLinkedList,
+  linkedListToArr,
+  printLinkedList
+};
