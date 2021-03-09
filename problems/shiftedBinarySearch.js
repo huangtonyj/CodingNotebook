7ce2e83753c6d51eafd52fec821186c6637d383d
tonyjhuang <huangtonyj91@gmail.com>
2021-03-08 22:07:32 -0800

    refactor

--- problems/shiftedBinarySearch.js
+++ problems/shiftedBinarySearch.js
@@ -1,27 +1,27 @@
 // ***
 function shiftedBinarySearch(arr, target, left = 0, right = arr.length - 1) {
-  const mid = Math.floor((right - left) / 2) + left;
+  if (left > right) return -1;
 
+  const mid = Math.floor((right - left) / 2) + left;
   const leftVal = arr[left];
   const midValue = arr[mid];
   const rightVal = arr[right];
 
-  if (left === mid && leftVal !== target) return -1;
   if (target === leftVal) return left;
   if (target === midValue) return mid;
   if (target === rightVal) return right;
 
   if (leftVal <= midValue) {
-    if (leftVal < target && target <= midValue) {
-      return shiftedBinarySearch(arr, target, left, mid);
+    if (leftVal <= target && target < midValue) {
+      return shiftedBinarySearch(arr, target, left, mid - 1);
     } else {
-      return shiftedBinarySearch(arr, target, mid, right);
+      return shiftedBinarySearch(arr, target, mid + 1, right);
     }
   } else if (midValue < rightVal) {
     if (midValue < target && target <= rightVal) {
-      return shiftedBinarySearch(arr, target, mid, right);
+      return shiftedBinarySearch(arr, target, mid + 1, right);
     } else {
-      return shiftedBinarySearch(arr, target, left, mid);
+      return shiftedBinarySearch(arr, target, left, mid - 1);
     }
   }
 }
