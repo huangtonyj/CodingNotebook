4a52d6a1170aeab853ec138455d11bece706bf3c
tonyjhuang <huangtonyj91@gmail.com>
2021-03-14 15:46:42 -0700

    refactor

--- problems/moveZeros.js
+++ problems/moveZeros.js
@@ -1,4 +1,31 @@
-function moveZeros(arr) {
+// O(n) time O(1) space
+function moveZeros(nums) {
+  const length = nums.length;
+  let zeroPointer = 0;
+  
+  while (nums[zeroPointer] !== 0 && zeroPointer < length) zeroPointer++;
+  
+  let pointer = zeroPointer;
+  
+  while (pointer < length) {
+    while (nums[pointer] === 0 && pointer < length) pointer++;
+    
+    if (pointer < length) {
+      _swap(nums, zeroPointer, pointer);
+    }
+
+    zeroPointer++;
+  }
+  
+  return nums;
+}
+
+function _swap(arr, idx1, idx2) {
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+}
+
+// O(n) time O(n) space
+function moveZeros1(arr) {
   let zerosCount = 0;
   const result = [];
 
