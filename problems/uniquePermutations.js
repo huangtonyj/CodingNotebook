e564c5ad2dd320980e791ece2cb8b2d535a60d08
tonyjhuang <huangtonyj91@gmail.com>
2021-03-16 22:01:32 -0700

    optimize

--- problems/uniquePermutations.js
+++ problems/uniquePermutations.js
@@ -1,3 +1,4 @@
+// ***
 /*
   Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
 
@@ -14,11 +15,40 @@
     Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 */
 
-function uniquePermutations(nums) {
+var uniquePermutations = function(nums) {
+  nums.sort((a, b) => a - b); // sort it once
+  
+  return _uniquePermutations(nums);
+};
+
+function _uniquePermutations(nums) {
+  if (nums.length === 1) return [nums];
+  
+  const result = [];
+  let prev = null;
+  
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (num === prev) continue;
+    prev = num;
+    
+    const nextNums = [...nums.slice(0, i), ...nums.slice(i + 1)];
+    const nextPerm = _uniquePermutations(nextNums);
+    
+    nextPerm.forEach((perm) => {
+      result.push([num, ...perm]);
+    });
+  }
+  
+  return result;
+}
+
+// Use a set to filter out duplicate permutations
+function uniquePermutations2(nums) {
   if (nums.length === 0) return [];
   if (nums.length === 1) return [nums];
   
-  let result = [];
+  const result = [];
   
   for (let i = 0; i < nums.length; i++) {
     const num = nums[i];
