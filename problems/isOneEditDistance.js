855bfa9b5027ef91e994455113766d9780ebaff6
tonyjhuang <huangtonyj91@gmail.com>
2021-03-19 00:50:54 -0700

    isOneEditDistance1()

---
+++ problems/isOneEditDistance.js
@@ -0,0 +1,43 @@
+const levenshteinDistance = require('./levenshteinDistance');
+
+/*
+  Given two strings s and t, 
+  return true if they are both one edit distance apart, 
+  otherwise return false.
+
+  A string s is said to be one distance apart from a string t if you can:
+
+  Insert exactly one character into s to get t.
+  Delete exactly one character from s to get t.
+  Replace exactly one character of s with a different character to get t.
+
+  Example 1:
+    Input: s = "ab", t = "acb"
+    Output: true
+    Explanation: We can insert 'c' into s to get t.
+
+  Example 2:
+    Input: s = "", t = ""
+    Output: false
+    Explanation: We cannot get t from s by only one step.
+
+  Example 3:
+    Input: s = "a", t = ""
+    Output: true
+
+  Example 4:
+    Input: s = "", t = "A"
+    Output: true
+*/
+
+// O(n) time O(min(n, m)) space
+function isOneEditDistance1(s, t) {
+  if (s.length === 1 && t.length === 1 && t !== s) return true;
+  return levenshteinDistance(s, t) === 1;
+}
+
+// console.log(isOneEditDistance('ab', 'acb'), true);
+// console.log(isOneEditDistance('a', ''), true);
+// console.log(isOneEditDistance('ab', 'ba'), false);
+// console.log(isOneEditDistance('cb', 'ab'), true);
+// console.log(isOneEditDistance('ab', 'acd'), false);
