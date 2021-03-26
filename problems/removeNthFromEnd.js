7af32abea1b549a12e0ce2de316bf4e32d4e5dae
tonyjhuang <huangtonyj91@gmail.com>
2021-03-25 23:46:41 -0700

    refactor

--- problems/removeNthFromEnd.js
+++ problems/removeNthFromEnd.js
@@ -1,44 +1,45 @@
-const NodeLL = require ('../dataStructures/nodeLL');
+const NodeLL = require("../dataStructures/nodeLL");
+
+// Note: use dummy head in case n = length of list
 
 function removeNthFromEnd(head, n) {
-  let currentNode = head;
-  let currentNodePlusN = head;
+  const dummy = new NodeLL(null);
+  dummy.next = head;
 
-  // Move pointer ahead by n nodes
-  for (let i = 0; i < n; i++) {
-    currentNodePlusN = currentNodePlusN.next;
-  }
-  
-  // Until pointer hits end aka null
-  while (currentNodePlusN.next) {
-    currentNode = currentNode.next;
-    currentNodePlusN = currentNodePlusN.next;
+  let slow = dummy;
+  let fast = dummy;
+
+  for (let i = 1; i <= n + 1; i++) {
+    fast = fast.next;
   }
 
-  // Remove next node
-  currentNode.next = currentNode.next.next;
+  while (fast) {
+    slow = slow.next;
+    fast = fast.next;
+  }
 
-  return head;
+  slow.next = slow.next.next;
+
+  return dummy.next;
 }
-
 
 // Test case
-const myLL = new NodeLL(10);
-  
-let currentNode = myLL;
-for (let i = 9; i > 0; i--) {
-  currentNode.next = new NodeLL(i);
-  currentNode = currentNode.next;
-}
+// const myLL = new NodeLL(10);
 
-currentNode = removeNthFromEnd(myLL, 3);
-let i = 10;
-while(currentNode) {
-  if (i !== 3) { 
-    console.log(currentNode.value === i, currentNode.value, i); 
-    currentNode = currentNode.next;
-  } else {
-    console.log(currentNode.value !== 3, currentNode.value, '2 not 3'); // deleted 3
-  }
-  i--;
-}
+// let currentNode = myLL;
+// for (let i = 9; i > 0; i--) {
+//   currentNode.next = new NodeLL(i);
+//   currentNode = currentNode.next;
+// }
+
+// currentNode = removeNthFromEnd(myLL, 3);
+// let i = 10;
+// while (currentNode) {
+//   if (i !== 3) {
+//     console.log(currentNode.value === i, currentNode.value, i);
+//     currentNode = currentNode.next;
+//   } else {
+//     console.log(currentNode.value !== 3, currentNode.value, "2 not 3"); // deleted 3
+//   }
+//   i--;
+// }
