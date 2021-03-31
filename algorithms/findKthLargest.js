b6aaf37b8c04cd1b2087e4607f3c6937b204fb7c
tonyjhuang <huangtonyj91@gmail.com>
2021-03-30 21:07:24 -0700

    findKthLargest()

---
+++ algorithms/findKthLargest.js
@@ -0,0 +1,14 @@
+const quickSelect = require('./quickSelect');
+
+function findKthLargest(arr, k) {
+  const length = arr.length;
+
+  return quickSelect(arr, length - k, 0, length - 1);
+}
+
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 1));
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2));
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 3));
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 4));
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 5));
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 6));
