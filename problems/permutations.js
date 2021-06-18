d014d3ce033c6871be2366117d672e0a14935848
tonyjhuang <huangtonyj91@gmail.com>
2021-06-18 11:19:59 -0700

    permutations backtrack

--- problems/permutations.js
+++ problems/permutations.js
@@ -1,10 +1,39 @@
-function permutations(arr) {
-  if (arr.length === 0) { return []; }
-  if (arr.length === 1) { return [arr]; }
+function permutations(array, inPermuation) {
+  if (inPermuation && inPermuation.every((el) => el === true)) return [[]];
 
   const result = [];
-  
-  for (let i = 0 ; i < arr.length ; i++) {
+  inPermuation = inPermuation || array.map((_) => false);
+
+  for (let i = 0; i < array.length; i++) {
+    if (inPermuation[i]) continue;
+
+    const val = array[i];
+
+    inPermuation[i] = true;
+
+    const nextPermuations = permutations(array, inPermuation);
+
+    for (const perm of nextPermuations) {
+      result.push([val, ...perm]);
+    }
+
+    inPermuation[i] = false;
+  }
+
+  return result;
+}
+
+function permutations2(arr) {
+  if (arr.length === 0) {
+    return [];
+  }
+  if (arr.length === 1) {
+    return [arr];
+  }
+
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
     const num = arr[i];
     const nextPerm = [...arr.slice(0, i), ...arr.slice(i + 1)];
     const perms = permutations(nextPerm);
