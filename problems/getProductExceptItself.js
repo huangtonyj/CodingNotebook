61d3cc492931fc7af2a31b200ec32d593686c0d6
tonyjhuang <huangtonyj91@gmail.com>
2023-09-08 15:18:15 -0700

    getProductExceptItself

---
+++ problems/getProductExceptItself.js
@@ -0,0 +1,15 @@
+// If our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24] . 
+// If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+function getProductExceptItself(array) {
+    const product = array.reduce((number, runningProduct) => number * runningProduct);
+    const result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        result.push(product / array[i]);
+    }
+
+    return result;
+}
+
+console.log(getProductExceptItself([1, 2, 3, 4, 5])); // [120, 60, 40, 30, 24]
