7e61da273f40d28d5b9e2718a29eb8a5e43de23c
Tony Huang <huangtonyj91@gmail.com>
2024-02-25 10:04:56 -0800

    flattenRecursively 2

--- problems/flattenRecursively.js
+++ problems/flattenRecursively.js
@@ -1,4 +1,4 @@
-function flattenRecursively(arr) {
+function flattenRecursively1(arr) {
   if (arr.length === 0) {
     return arr;
   }
@@ -12,5 +12,25 @@ function flattenRecursively(arr) {
   }
 }
 
+function flattenRecursively(arr) {
+  const flattened = [];
+
+  arr.forEach((el) => {
+    if (Array.isArray(el)) {
+      const nextFlattened = flattenRecursively(el);
+      nextFlattened.forEach((el2) => {
+        flattened.push(el2);
+      });
+    } else {
+      flattened.push(el);
+    }
+  });
+
+  return flattened;
+}
+
+console.log(flattenRecursively([1, 2, 3, 4])); //-> [1, 2, 3, 4]
 console.log(flattenRecursively([1, [2, 3, [4]]])); //-> [1, 2, 3, 4]
 console.log(flattenRecursively([1, {}, [3, [[4]]]])); //-> [1, {}, 3, 4]
+console.log(flattenRecursively([1, {}, [3, [[4, 5]]]])); //-> [1, {}, 3, 4, 5]
+console.log(flattenRecursively([1, {}, [3, [[4, 5, [[[[[[[6]]]]]]]]]]])); //-> [1, {}, 3, 4, 5, 6]
