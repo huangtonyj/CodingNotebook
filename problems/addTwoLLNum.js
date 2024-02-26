5e0c78087d4efc0762165ded65e55cfbcf7c5c76
Tony Huang <huangtonyj91@gmail.com>
2024-02-25 23:42:44 -0800

    refactor

--- problems/addTwoLLNum.js
+++ problems/addTwoLLNum.js
@@ -1,16 +1,17 @@
-// Given two non-empty linked lists representing two non-negative integers. 
-// The digits are stored in reverse order and each of their nodes contain a single digit. 
+// Given two non-empty linked lists representing two non-negative integers.
+// The digits are stored in reverse order and each of their nodes contain a single digit.
 // Add the two numbers and return it as a linked list.
 
 // Input: (2 - > 4 - > 3) + (5 - > 6 - > 4 -> 1)
 // Output: 7 - > 0 - > 8 -> 1
 // Explanation: 342 + 465 = 1807.
 
-const nodeLL = require('../dataStructures/NodeLL');
+const nodeLL = require("../dataStructures/NodeLL");
 
 const {
-  arrayToLinkedList, linkedListToArr
-} = require('../algorithms/linkedListUtils');
+  arrayToLinkedList,
+  linkedListToArr,
+} = require("../algorithms/linkedListUtils");
 
 // Returns a number
 function addTwoLLNum(LL1, LL2) {
@@ -38,28 +39,30 @@ function addTwoLLNum(LL1, LL2) {
 function addTwoLLNumLL(node1, node2) {
   let dummyHead = new nodeLL(null);
   let currentNode = dummyHead;
+  let currentNode1 = node1;
+  let currentNode2 = node2;
   let carry = 0;
-  
-  while (node1 || node2) {
-      const valueOne = node1 && node1.value || 0;
-      const valueTwo = node2 && node2.value || 0;
-      const sum = valueOne + valueTwo + carry;
-      carry = Math.floor(sum / 10);
 
-      currentNode.next = new nodeLL(sum % 10);
+  while (currentNode1 || currentNode2) {
+    const valueOne = (currentNode1 && currentNode1.value) || 0;
+    const valueTwo = (currentNode2 && currentNode2.value) || 0;
+    const sum = valueOne + valueTwo + carry;
+    carry = sum > 9 ? 1 : 0;
 
-      currentNode = currentNode.next;
-      node1 = node1 && node1.next;
-      node2 = node2 && node2.next;
+    currentNode.next = new nodeLL(sum % 10);
+
+    currentNode = currentNode.next;
+    currentNode1 = currentNode1 && currentNode1.next;
+    currentNode2 = currentNode2 && currentNode2.next;
   }
-  
+
   if (carry) currentNode.next = new nodeLL(carry);
-  
+
   return dummyHead.next;
 }
 
-// const LL1 = arrayToLinkedList([2, 4, 3]);
-// const LL2 = arrayToLinkedList([5, 6, 4, 1]);
+const LL1 = arrayToLinkedList([2, 4, 3]);
+const LL2 = arrayToLinkedList([5, 6, 4, 1]);
 
 // console.log(addTwoLLNum(LL1, LL2), 1807);
-// console.log(linkedListToArr(addTwoLLNumLL(LL1, LL2)), [7, 0, 8 ,1]);
+console.log(linkedListToArr(addTwoLLNumLL(LL1, LL2)), [7, 0, 8, 1]);
