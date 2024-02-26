1aff747d4cdd814cd4b5257a710b69f217c9c905
Tony Huang <huangtonyj91@gmail.com>
2024-02-26 12:50:36 -0800

    getIntersectionNode2

--- problems/getIntersectionNode.js
+++ problems/getIntersectionNode.js
@@ -31,3 +31,39 @@ function getIntersectionNode(headA, headB) {
 
   return null;
 }
+
+// O(2N + 2M) time, O(1) space
+// The last section of the list are the same
+// Fast forward longer list to same length
+// Increment both and check if same
+function getIntersectionNode2(headA, headB) {
+  let currentA = headA;
+  let currentB = headB;
+  let lengthA = 0;
+  let lengthB = 0;
+
+  while (currentA) {
+    lengthA++;
+    currentA = currentA.next;
+  }
+
+  while (currentB) {
+    lengthB++;
+    currentB = currentB.next;
+  }
+
+  let longer = lengthA > lengthB ? headA : headB;
+  let shorter = lengthA > lengthB ? headB : headA;
+
+  for (let i = 0; i < Math.abs(lengthA - lengthB); i++) {
+    longer = longer.next;
+  }
+
+  while (longer || shorter) {
+    if (longer == shorter) return longer;
+    longer = longer.next;
+    shorter = shorter.next;
+  }
+
+  return null;
+}
