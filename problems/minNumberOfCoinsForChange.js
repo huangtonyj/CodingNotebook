6b822aa366fc68c3d3c7caeec77f89e17fc2dca6
Tony Huang <huangtonyj91@gmail.com>
2024-02-28 00:10:27 -0800

    minNumberOfCoinsForChange3

--- problems/minNumberOfCoinsForChange.js
+++ problems/minNumberOfCoinsForChange.js
@@ -1,11 +1,11 @@
 // ***
-function minNumberOfCoinsForChange(n, denoms) {
-  if  (n === 0) return 0;
+function minNumberOfCoinsForChange1(n, denoms) {
+  if (n === 0) return 0;
 
   const queue = denoms.map((denom) => {
     return {
-      total: denom, 
-      count: 1
+      total: denom,
+      count: 1,
     };
   });
 
@@ -13,20 +13,22 @@ function minNumberOfCoinsForChange(n, denoms) {
 
   while (current.total !== n && queue.length) {
     current = queue.shift();
-    if (current.total > n) { continue; }
+    if (current.total > n) {
+      continue;
+    }
 
     for (let denom of denoms) {
       const total = current.total + denom;
       const count = current.count + 1;
 
-      queue.push({total, count});
-      // console.log(current, queue);
+      queue.push({ total, count });
     }
-  } 
+  }
 
   return queue.length ? current.count : -1;
 }
 
+// Bottom Up
 function minNumberOfCoinsForChange2(n, denoms) {
   const numOfCoins = new Array(n + 1).fill(Infinity);
   numOfCoins[0] = 0;
@@ -34,8 +36,10 @@ function minNumberOfCoinsForChange2(n, denoms) {
   for (const denom of denoms) {
     for (let amount = 0; amount < numOfCoins.length; amount++) {
       if (denom <= amount) {
-        numOfCoins[amount] = Math.min(numOfCoins[amount], numOfCoins[amount - denom] + 1);
-        // console.log(denom, numOfCoins);
+        numOfCoins[amount] = Math.min(
+          numOfCoins[amount],
+          numOfCoins[amount - denom] + 1
+        );
       }
     }
   }
@@ -43,11 +47,30 @@ function minNumberOfCoinsForChange2(n, denoms) {
   return numOfCoins[n] !== Infinity ? numOfCoins[n] : -1;
 }
 
+// Top down (backtracking with memoization)
+function minNumberOfCoinsForChange3(coins, amount) {
+  const _coinChange = (amount, coins, memo = {}) => {
+    if (amount == 0) return 0;
+    if (amount < 0) return Infinity;
+    if (amount in memo) return memo[amount];
 
-module.exports = minNumberOfCoinsForChange;
+    const numOfCoinsArr = coins.map((coin) =>
+      _coinChange(amount - coin, coins, memo)
+    );
+    const numOfCoins = 1 + Math.min(...numOfCoinsArr);
 
-// Simply a BFS search
-// Improvement: Add a set to track current.total that was seen before.
+    memo[amount] = numOfCoins; // cache it for future iterations
 
-// minNumberOfCoinsForChange(25, [1, 5, 10]);
-// minNumberOfCoinsForChange2(25, [1, 5, 10]);
+    return numOfCoins;
+  };
+
+  const numOfCoins = _coinChange(coins, amount);
+
+  return numOfCoins == Infinity ? -1 : numOfCoins;
+}
+
+module.exports = minNumberOfCoinsForChange1;
+
+// console.log(minNumberOfCoinsForChange(25, [1, 5, 10]));
+// console.log(minNumberOfCoinsForChange2(25, [1, 5, 10]));
+// console.log(minNumberOfCoinsForChange3(25, [1, 5, 10]));
